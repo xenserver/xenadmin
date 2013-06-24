aa5fe93ff54b1df9239ccde8be170c2aeeba2396
Mihaela Stoica <mihaela.stoica@citrix.com>
2013-06-24 17:44:25 +0100

    CP-4818: Change permissions to id_dsa file in script
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- mk/bumpBuildNumber.sh
+++ mk/bumpBuildNumber.sh
@@ -49,6 +49,8 @@ fi
 
 ssh_key="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/id_dsa"
 
+chmod 600 ${ssh_key}
+
 PSQL="ssh -i ${ssh_key} -v xenbuild@xenbuilder.uk.xensource.com PGPASSWORD=xenadmindb psql -q -A -t xenbuilder xenadmin"
 
 QUERY="""INSERT INTO xenadmin_builds (build_number,job,revision) SELECT ${get_BUILD_NUMBER},'${get_JOB_NAME}','${get_GIT_REVISION}' WHERE NOT EXISTS ( SELECT 1 FROM xenadmin_builds WHERE build_number = ${get_BUILD_NUMBER});
