8217db7a7390a4f865027cbddc5324caa27b04a2
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2013-09-26 14:39:27 +0100

    CP-6317: Importing from branch clearwater-ln. # HG changeset patch # User Konstantina Chremmou <Konstantina.Chremmou@citrix.com> # Date 1377021015 -3600 # Node ID 0d5b08d2db1b4c8e5d656750e2ee4bcedc3d5579 # Parent  7674677d207c53d4a1207a88d0f1aa82c68b22fe CA-11234: Layout corrections to the SummaryPanel.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Controls/SummaryPanel/SummaryPanel.Designer.cs
+++ XenAdmin/Controls/SummaryPanel/SummaryPanel.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SummaryPanel));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.warningIcon = new System.Windows.Forms.PictureBox();
@@ -47,137 +48,75 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.titleLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.information, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.helperLink, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.informationLayoutPanel, 0, 3);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.helperLink, 0, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(235, 255);
-            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 183F));
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
             this.tableLayoutPanel2.Controls.Add(this.warningIcon, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.warningText, 1, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 10);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(229, 41);
-            this.tableLayoutPanel2.TabIndex = 2;
             // 
             // warningIcon
             // 
-            this.warningIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.warningIcon.Location = new System.Drawing.Point(3, 3);
+            resources.ApplyResources(this.warningIcon, "warningIcon");
             this.warningIcon.Name = "warningIcon";
-            this.warningIcon.Size = new System.Drawing.Size(40, 35);
-            this.warningIcon.TabIndex = 0;
             this.warningIcon.TabStop = false;
             // 
             // warningText
             // 
-            this.warningText.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            resources.ApplyResources(this.warningText, "warningText");
             this.warningText.AutoEllipsis = true;
-            this.warningText.AutoSize = true;
-            this.warningText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.warningText.Location = new System.Drawing.Point(49, 14);
             this.warningText.Name = "warningText";
-            this.warningText.Size = new System.Drawing.Size(0, 13);
-            this.warningText.TabIndex = 1;
             // 
             // titleLabel
             // 
-            this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            resources.ApplyResources(this.titleLabel, "titleLabel");
             this.titleLabel.AutoEllipsis = true;
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(8, 13);
-            this.titleLabel.Margin = new System.Windows.Forms.Padding(8, 8, 3, 8);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(0, 13);
-            this.titleLabel.TabIndex = 4;
             // 
             // information
             // 
-            this.information.AutoSize = true;
-            this.information.Location = new System.Drawing.Point(8, 48);
-            this.information.Margin = new System.Windows.Forms.Padding(8);
+            resources.ApplyResources(this.information, "information");
             this.information.Name = "information";
-            this.information.Size = new System.Drawing.Size(0, 13);
-            this.information.TabIndex = 5;
             // 
             // helperLink
             // 
             this.helperLink.AutoEllipsis = true;
-            this.helperLink.AutoSize = true;
-            this.helperLink.Location = new System.Drawing.Point(8, 270);
-            this.helperLink.Margin = new System.Windows.Forms.Padding(8, 0, 0, 8);
+            resources.ApplyResources(this.helperLink, "helperLink");
             this.helperLink.Name = "helperLink";
-            this.helperLink.Size = new System.Drawing.Size(0, 13);
-            this.helperLink.TabIndex = 3;
             // 
             // informationLayoutPanel
             // 
-            this.informationLayoutPanel.ColumnCount = 2;
-            this.informationLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 39F));
-            this.informationLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            resources.ApplyResources(this.informationLayoutPanel, "informationLayoutPanel");
             this.informationLayoutPanel.Controls.Add(this.informationImage, 0, 0);
             this.informationLayoutPanel.Controls.Add(this.informationMessage, 1, 0);
-            this.informationLayoutPanel.Location = new System.Drawing.Point(3, 119);
             this.informationLayoutPanel.Name = "informationLayoutPanel";
-            this.informationLayoutPanel.RowCount = 1;
-            this.informationLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.informationLayoutPanel.Size = new System.Drawing.Size(229, 40);
-            this.informationLayoutPanel.TabIndex = 6;
             // 
             // informationImage
             // 
-            this.informationImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.informationImage.Location = new System.Drawing.Point(3, 3);
+            resources.ApplyResources(this.informationImage, "informationImage");
             this.informationImage.Name = "informationImage";
-            this.informationImage.Size = new System.Drawing.Size(33, 34);
-            this.informationImage.TabIndex = 0;
             this.informationImage.TabStop = false;
             // 
             // informationMessage
             // 
-            this.informationMessage.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            resources.ApplyResources(this.informationMessage, "informationMessage");
             this.informationMessage.AutoEllipsis = true;
-            this.informationMessage.AutoSize = true;
-            this.informationMessage.Location = new System.Drawing.Point(42, 13);
             this.informationMessage.Name = "informationMessage";
-            this.informationMessage.Size = new System.Drawing.Size(0, 13);
-            this.informationMessage.TabIndex = 1;
             // 
             // SummaryPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "SummaryPanel";
-            this.Size = new System.Drawing.Size(235, 295);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
