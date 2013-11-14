37a32615dc5f64cc18fe81c92935ce177aab75ea
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2013-11-14 10:38:37 +0000

    CP-6382: Internationalized GPU page on Pool properties dialog (PR-1675) # HG changeset patch # User Mihaela Stoica <Mihaela.Stoica@citrix.com> # Date 1381487805 -3600 #      Fri Oct 11 11:36:45 2013 +0100 # Node ID 500886f75c7f678b98ad5fab5902a0e87d1798ea # Parent  ae162fb56bac4f1c2f7cc4c47b640a6933ea6deb
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/SettingsPanels/PoolGpuEditPage.Designer.cs
+++ XenAdmin/SettingsPanels/PoolGpuEditPage.Designer.cs
@@ -28,94 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PoolGpuEditPage));
             this.labelRubric = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButtonMixture = new System.Windows.Forms.RadioButton();
             this.radioButtonDepth = new System.Windows.Forms.RadioButton();
             this.radioButtonBreadth = new System.Windows.Forms.RadioButton();
-            this.radioButtonMixture = new System.Windows.Forms.RadioButton();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelRubric
             // 
-            this.labelRubric.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelRubric.AutoSize = true;
-            this.labelRubric.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelRubric.Location = new System.Drawing.Point(3, 0);
+            resources.ApplyResources(this.labelRubric, "labelRubric");
             this.labelRubric.Name = "labelRubric";
-            this.labelRubric.Padding = new System.Windows.Forms.Padding(0, 3, 0, 10);
-            this.labelRubric.Size = new System.Drawing.Size(412, 39);
-            this.labelRubric.TabIndex = 2;
-            this.labelRubric.Text = "Set a placement policy for assigning VMs to GPUs to achieve either maximum densit" +
-                "y or maximum performance.";
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.radioButtonMixture, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.labelRubric, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.radioButtonDepth, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.radioButtonBreadth, 0, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(418, 289);
-            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // radioButtonMixture
+            // 
+            resources.ApplyResources(this.radioButtonMixture, "radioButtonMixture");
+            this.radioButtonMixture.Name = "radioButtonMixture";
+            this.radioButtonMixture.TabStop = true;
+            this.radioButtonMixture.UseVisualStyleBackColor = true;
             // 
             // radioButtonDepth
             // 
-            this.radioButtonDepth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.radioButtonDepth.AutoSize = true;
-            this.radioButtonDepth.Location = new System.Drawing.Point(3, 42);
+            resources.ApplyResources(this.radioButtonDepth, "radioButtonDepth");
             this.radioButtonDepth.Name = "radioButtonDepth";
-            this.radioButtonDepth.Size = new System.Drawing.Size(412, 17);
-            this.radioButtonDepth.TabIndex = 3;
             this.radioButtonDepth.TabStop = true;
-            this.radioButtonDepth.Text = "Maximum density: put as many VMs as possible on the same GPU";
             this.radioButtonDepth.UseVisualStyleBackColor = true;
             // 
             // radioButtonBreadth
             // 
-            this.radioButtonBreadth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.radioButtonBreadth.AutoSize = true;
-            this.radioButtonBreadth.Location = new System.Drawing.Point(3, 65);
+            resources.ApplyResources(this.radioButtonBreadth, "radioButtonBreadth");
             this.radioButtonBreadth.Name = "radioButtonBreadth";
-            this.radioButtonBreadth.Size = new System.Drawing.Size(412, 17);
-            this.radioButtonBreadth.TabIndex = 4;
             this.radioButtonBreadth.TabStop = true;
-            this.radioButtonBreadth.Text = "Maximum performance: put VMs on as many GPUs as possible";
             this.radioButtonBreadth.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonMixture
-            // 
-            this.radioButtonMixture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.radioButtonMixture.AutoSize = true;
-            this.radioButtonMixture.Enabled = false;
-            this.radioButtonMixture.Location = new System.Drawing.Point(3, 88);
-            this.radioButtonMixture.Name = "radioButtonMixture";
-            this.radioButtonMixture.Size = new System.Drawing.Size(412, 17);
-            this.radioButtonMixture.TabIndex = 5;
-            this.radioButtonMixture.TabStop = true;
-            this.radioButtonMixture.Text = "Unknown or mixture";
-            this.radioButtonMixture.UseVisualStyleBackColor = true;
             // 
             // PoolGpuEditPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "PoolGpuEditPage";
-            this.Size = new System.Drawing.Size(418, 289);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
