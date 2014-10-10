7ea996f9bdc0b38a9765d59620dd59521f6d2f09
Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
2014-10-10 11:28:09 +0100

    CA-148374: Progress action disappears in Events on Dismiss all -Also fixed the Dismiss Selected button
    Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>

--- XenAdmin/TabPages/HistoryPage.cs
+++ XenAdmin/TabPages/HistoryPage.cs
@@ -405,13 +405,7 @@ namespace XenAdmin.TabPages
                     return;
             }
 
-            var actions = new List<ActionBase>();
-            foreach (var row in dataGridView.SelectedRows)
-            {
-                var actionRow = row as DataGridViewActionRow;
-                if (actionRow != null)
-                    actions.Add(actionRow.Action);
-            }
+            var actions = from DataGridViewActionRow row in dataGridView.SelectedRows where row != null && row.Action != null && row.Action.IsCompleted select row.Action;
 
             ConnectionsManager.History.RemoveAll(actions.Contains);
         }
