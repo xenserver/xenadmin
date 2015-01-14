7fe521faa5a53d34eacb471f515cb15f699437b3
Mihaela Stoica <mihaela.stoica@citrix.com>
2015-01-14 17:01:50 +0000

    CA-147163: Make new form localizable and fix tests
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/Wizards/PatchingWizard/PatchingWizard_UploadPage.Designer.cs
+++ XenAdmin/Wizards/PatchingWizard/PatchingWizard_UploadPage.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatchingWizard_UploadPage));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label2 = new System.Windows.Forms.Label();
             this.labelProgress = new System.Windows.Forms.Label();
@@ -38,81 +39,44 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.labelProgress, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.flickerFreeListBox1, 0, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 397);
-            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // label2
             // 
-            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(700, 37);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "XenCenter is now uploading your update to the servers specified in the previous s" +
-    "tep.\r\nPlease wait for this operation to complete, then click Next to continue wi" +
-    "th the installation.";
             // 
             // labelProgress
             // 
-            this.labelProgress.AutoSize = true;
-            this.labelProgress.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelProgress.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelProgress.Location = new System.Drawing.Point(3, 357);
-            this.labelProgress.Margin = new System.Windows.Forms.Padding(3);
+            resources.ApplyResources(this.labelProgress, "labelProgress");
             this.labelProgress.Name = "labelProgress";
-            this.labelProgress.Padding = new System.Windows.Forms.Padding(1);
-            this.labelProgress.Size = new System.Drawing.Size(694, 15);
-            this.labelProgress.TabIndex = 0;
-            this.labelProgress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // progressBar1
             // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.progressBar1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.progressBar1.Location = new System.Drawing.Point(3, 378);
+            resources.ApplyResources(this.progressBar1, "progressBar1");
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(694, 16);
-            this.progressBar1.TabIndex = 1;
             // 
             // flickerFreeListBox1
             // 
-            this.flickerFreeListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            resources.ApplyResources(this.flickerFreeListBox1, "flickerFreeListBox1");
             this.flickerFreeListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.flickerFreeListBox1.FormattingEnabled = true;
-            this.flickerFreeListBox1.IntegralHeight = false;
-            this.flickerFreeListBox1.ItemHeight = 18;
             this.flickerFreeListBox1.Items.AddRange(new object[] {
-            "Nothing to upload"});
-            this.flickerFreeListBox1.Location = new System.Drawing.Point(3, 40);
+            resources.GetString("flickerFreeListBox1.Items")});
             this.flickerFreeListBox1.Name = "flickerFreeListBox1";
-            this.flickerFreeListBox1.Size = new System.Drawing.Size(694, 311);
-            this.flickerFreeListBox1.TabIndex = 2;
             this.flickerFreeListBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.flickerFreeListBox1_DrawItem);
             // 
             // PatchingWizard_UploadPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "PatchingWizard_UploadPage";
-            this.Size = new System.Drawing.Size(700, 397);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
