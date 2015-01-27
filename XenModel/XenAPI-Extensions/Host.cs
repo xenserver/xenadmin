355dd63ae77dd94e57ae94380f1f7743166e6370
Mihaela Stoica <mihaela.stoica@citrix.com>
2015-01-27 12:37:13 +0000

    CP-10824: Supp Pack installation through XenCenter: Removed comment
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenModel/XenAPI-Extensions/Host.cs
+++ XenModel/XenAPI-Extensions/Host.cs
@@ -1519,7 +1519,7 @@ namespace XenAPI
         {
             get
             {
-                return Helpers.CreamOrGreater(Connection);  // TODO: Replace with CreamOrGreater
+                return Helpers.CreamOrGreater(Connection);
             }
         }
         #endregion
