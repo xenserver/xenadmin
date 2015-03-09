fd29a1b53cccb54867d3d979cf6026b3a37f0876
Mihaela Stoica <mihaela.stoica@citrix.com>
2015-03-09 15:43:49 +0000

    CA-162992: Better display of container network port mapping information on General tab; changes following code review
    - do not add the Ports entry if it is empty
    
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/TabPages/GeneralTabPage.cs
+++ XenAdmin/TabPages/GeneralTabPage.cs
@@ -1373,7 +1373,10 @@ namespace XenAdmin.TabPages
                 s.AddEntry(Messages.CONTAINER, dockerContainer.container.Length != 0 ? dockerContainer.container : Messages.NONE);
                 s.AddEntry(Messages.CONTAINER_COMMAND, dockerContainer.command.Length != 0 ? dockerContainer.command : Messages.NONE);
                 var ports = dockerContainer.PortList.Select(p => p.Description);
-                s.AddEntry(Messages.CONTAINER_PORTS, ports.Count() != 0 ? string.Join("\n", ports) : Messages.NONE);
+                if (ports.Count() > 0)
+                {
+                    s.AddEntry(Messages.CONTAINER_PORTS, string.Join(Environment.NewLine, ports));
+                }
                 s.AddEntry(Messages.UUID, dockerContainer.uuid.Length != 0 ? dockerContainer.uuid : Messages.NONE);
             }
         }
