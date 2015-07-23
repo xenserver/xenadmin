ad154e2ec7664a064d9f08a91d50ed3d22ee8735
Jenkins <jenkins@tocco.do.citrite.net>
2015-07-23 17:03:24 +0100

    Added locking for execution of gui tests.

--- mk/tests-checks.sh
+++ mk/tests-checks.sh
@@ -48,8 +48,23 @@ cd ${TEST_DIR} && tar xzf XenAdminTests.tgz && chmod -R 777 Release
 
 
 set +e
-# /output="$(cygpath -d ${TEST_DIR})\output.nunit.log"
-nunit-console /nologo /nodots /process=separate /noshadow /labels /err="$(cygpath -d ${TEST_DIR})\error.nunit.log" /timeout=40000 /xml="$(cygpath -d ${TEST_DIR})\XenAdminTests.xml" "$(cygpath -d ${TEST_DIR})\Release\XenAdminTests.dll" "/framework=net-4.5" &
+
+{
+  # failure timeout COUNTE*DELAY = 600 seconds
+  COUNTER=60
+  DELAY=10
+  echo -en "INFO:       Aquiring GUI testing lock "
+  until [  $COUNTER -lt 1 ]; do
+     flock --conflict-exit-code 123 -w $DELAY 200
+     RESULT=$?
+     if [ "$RESULT" == "123" ]; then echo -n "."; fi
+     if [ "$?" != "0" ]; then echo "\nERROR:    Failed to aquire lock, timeout exceeded."; exit 1; fi
+     let COUNTER-=1
+  done
+
+  echo -e "\nINFO:	Lock aquired and starting execution"
+   # /output="$(cygpath -d ${TEST_DIR})\output.nunit.log"
+nunit-console /nologo /labels /stoponerror /nodots /process=separate /noshadow /labels /err="$(cygpath -d ${TEST_DIR})\error.nunit.log" /timeout=40000 /xml="$(cygpath -d ${TEST_DIR})\XenAdminTests.xml" "$(cygpath -d ${TEST_DIR})\Release\XenAdminTests.dll" "/framework=net-4.5" &
 
 pid=$!
 (sleep 3000 ; kill $pid 2>/dev/null ) &
@@ -59,6 +74,11 @@ if [ $? = 143 ]
 then
   echo "Tests were terminated due to 3000 timeout."
 fi
+
+  echo "INFO:   Done, releasing lock"
+  
+} 200>/tmp/busy-gui.log
+
 set -e
 
 sleeperpid2=$((ps | grep ${sleeperpid} | grep 'sleep$' | cut -b-10) || true)
