8898f6b69c0b0a9b2ee72a0679e0fd9b032ecbc2
Mihaela Stoica <mihaela.stoica@citrix.com>
2015-08-17 14:47:55 +0100

    CP-13287: Add "Tech Preview" warning to Intel GVT-g vGPUs
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/SettingsPanels/GpuEditPage.Designer.cs
+++ XenAdmin/SettingsPanels/GpuEditPage.Designer.cs
@@ -44,6 +44,8 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.labelGpuType = new System.Windows.Forms.Label();
             this.comboBoxGpus = new XenAdmin.Controls.VgpuComboBox();
+            this.imgExperimental = new System.Windows.Forms.PictureBox();
+            this.labelExperimental = new System.Windows.Forms.Label();
             this.warningsTable.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgRDP)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgNeedDriver)).BeginInit();
@@ -51,6 +53,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.imgStopVM)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgHA)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgExperimental)).BeginInit();
             this.SuspendLayout();
             // 
             // labelRubric
@@ -61,39 +64,37 @@
             // warningsTable
             // 
             resources.ApplyResources(this.warningsTable, "warningsTable");
-            this.warningsTable.Controls.Add(this.imgRDP, 0, 0);
-            this.warningsTable.Controls.Add(this.labelRDP, 1, 0);
-            this.warningsTable.Controls.Add(this.imgNeedDriver, 0, 1);
-            this.warningsTable.Controls.Add(this.labelNeedDriver, 1, 1);
-            this.warningsTable.Controls.Add(this.imgNeedGpu, 0, 2);
-            this.warningsTable.Controls.Add(this.labelNeedGpu, 1, 2);
-            this.warningsTable.Controls.Add(this.imgStopVM, 0, 3);
-            this.warningsTable.Controls.Add(this.labelStopVM, 1, 3);
-            this.warningsTable.Controls.Add(this.imgHA, 0, 4);
-            this.warningsTable.Controls.Add(this.labelHA, 1, 4);
+            this.warningsTable.Controls.Add(this.imgRDP, 0, 1);
+            this.warningsTable.Controls.Add(this.labelRDP, 1, 1);
+            this.warningsTable.Controls.Add(this.imgNeedDriver, 0, 2);
+            this.warningsTable.Controls.Add(this.labelNeedDriver, 1, 2);
+            this.warningsTable.Controls.Add(this.imgNeedGpu, 0, 3);
+            this.warningsTable.Controls.Add(this.labelNeedGpu, 1, 3);
+            this.warningsTable.Controls.Add(this.imgStopVM, 0, 4);
+            this.warningsTable.Controls.Add(this.labelStopVM, 1, 4);
+            this.warningsTable.Controls.Add(this.imgHA, 0, 5);
+            this.warningsTable.Controls.Add(this.labelHA, 1, 5);
+            this.warningsTable.Controls.Add(this.imgExperimental, 0, 0);
+            this.warningsTable.Controls.Add(this.labelExperimental, 1, 0);
             this.warningsTable.Name = "warningsTable";
             this.warningsTable.SizeChanged += new System.EventHandler(this.warningsTable_SizeChanged);
             // 
             // imgRDP
             // 
-            this.imgRDP.ErrorImage = null;
-            this.imgRDP.Image = global::XenAdmin.Properties.Resources._000_Alert2_h32bit_16;
             resources.ApplyResources(this.imgRDP, "imgRDP");
-            this.imgRDP.InitialImage = null;
+            this.imgRDP.Image = global::XenAdmin.Properties.Resources._000_Alert2_h32bit_16;
             this.imgRDP.Name = "imgRDP";
             this.imgRDP.TabStop = false;
             // 
             // labelRDP
             // 
             resources.ApplyResources(this.labelRDP, "labelRDP");
-            this.labelRDP.MaximumSize = new System.Drawing.Size(586, 999);
             this.labelRDP.Name = "labelRDP";
             // 
             // imgNeedDriver
             // 
             this.imgNeedDriver.Image = global::XenAdmin.Properties.Resources._000_Alert2_h32bit_16;
             resources.ApplyResources(this.imgNeedDriver, "imgNeedDriver");
-            this.imgNeedDriver.InitialImage = null;
             this.imgNeedDriver.Name = "imgNeedDriver";
             this.imgNeedDriver.TabStop = false;
             // 
@@ -104,10 +105,8 @@
             // 
             // imgNeedGpu
             // 
-            this.imgNeedGpu.ErrorImage = null;
-            this.imgNeedGpu.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             resources.ApplyResources(this.imgNeedGpu, "imgNeedGpu");
-            this.imgNeedGpu.InitialImage = null;
+            this.imgNeedGpu.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             this.imgNeedGpu.Name = "imgNeedGpu";
             this.imgNeedGpu.TabStop = false;
             // 
@@ -118,32 +117,26 @@
             // 
             // imgStopVM
             // 
-            this.imgStopVM.ErrorImage = null;
-            this.imgStopVM.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             resources.ApplyResources(this.imgStopVM, "imgStopVM");
-            this.imgStopVM.InitialImage = null;
+            this.imgStopVM.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             this.imgStopVM.Name = "imgStopVM";
             this.imgStopVM.TabStop = false;
             // 
             // labelStopVM
             // 
             resources.ApplyResources(this.labelStopVM, "labelStopVM");
-            this.labelStopVM.MaximumSize = new System.Drawing.Size(584, 999);
             this.labelStopVM.Name = "labelStopVM";
             // 
             // imgHA
             // 
-            this.imgHA.ErrorImage = null;
-            this.imgHA.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             resources.ApplyResources(this.imgHA, "imgHA");
-            this.imgHA.InitialImage = null;
+            this.imgHA.Image = global::XenAdmin.Properties.Resources._000_Info3_h32bit_16;
             this.imgHA.Name = "imgHA";
             this.imgHA.TabStop = false;
             // 
             // labelHA
             // 
             resources.ApplyResources(this.labelHA, "labelHA");
-            this.labelHA.MaximumSize = new System.Drawing.Size(584, 999);
             this.labelHA.Name = "labelHA";
             // 
             // tableLayoutPanel1
@@ -167,6 +160,18 @@
             this.comboBoxGpus.Name = "comboBoxGpus";
             this.comboBoxGpus.SelectedIndexChanged += new System.EventHandler(this.comboBoxGpus_SelectedIndexChanged);
             // 
+            // imgExperimental
+            // 
+            this.imgExperimental.Image = global::XenAdmin.Properties.Resources._000_Alert2_h32bit_16;
+            resources.ApplyResources(this.imgExperimental, "imgExperimental");
+            this.imgExperimental.Name = "imgExperimental";
+            this.imgExperimental.TabStop = false;
+            // 
+            // labelExperimental
+            // 
+            resources.ApplyResources(this.labelExperimental, "labelExperimental");
+            this.labelExperimental.Name = "labelExperimental";
+            // 
             // GpuEditPage
             // 
             resources.ApplyResources(this, "$this");
@@ -184,6 +189,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.imgHA)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgExperimental)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,5 +212,7 @@
         private System.Windows.Forms.Label labelNeedDriver;
         private System.Windows.Forms.PictureBox imgHA;
         private System.Windows.Forms.Label labelHA;
+        private System.Windows.Forms.PictureBox imgExperimental;
+        private System.Windows.Forms.Label labelExperimental;
     }
 }
