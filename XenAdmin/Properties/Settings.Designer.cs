49e07882a68782c95f3ce572576eb4fe3d0ee882
Mihaela Stoica <mihaela.stoica@citrix.com>
2016-02-01 11:21:17 +0000

    CP-15825: Set Roaming=true for the new setting
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/Properties/Settings.Designer.cs
+++ XenAdmin/Properties/Settings.Designer.cs
@@ -699,6 +699,7 @@ namespace XenAdmin.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         public bool ShowAboutDialog {
             get {
                 return ((bool)(this["ShowAboutDialog"]));
