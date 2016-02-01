74a759c44d70cc3bb2bc72580a32c1bd4774f3b1
Mihaela Stoica <Mihaela.Stoica@citrix.com>
2016-02-01 14:26:04 +0000

    CP-15405: Add script for signing and packaging
    Signed-off-by: Mihaela Stoica <Mihaela.Stoica@citrix.com>

---
+++ mk/package-and-sign.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Copyright (c) Citrix Systems Inc. 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, 
+# with or without modification, are permitted provided 
+# that the following conditions are met: 
+# 
+# *   Redistributions of source code must retain the above 
+#     copyright notice, this list of conditions and the 
+#     following disclaimer. 
+# *   Redistributions in binary form must reproduce the above 
+#     copyright notice, this list of conditions and the 
+#     following disclaimer in the documentation and/or other 
+#     materials provided with the distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+# SUCH DAMAGE.
+
+set -eu
+
+ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+XENADMIN_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+REPO=${XENADMIN_DIR}
+OUTPUT_DIR=${ROOT}/output
+DOTNETINST=${REPO}/dotNetInstaller
+	
+
+
+WIX=${REPO}/WixInstaller
+WIX_BIN=${WIX}/bin
+
+CANDLE="candle.exe -nologo"
+LIT="lit.exe -nologo"
+LIGHT="light.exe -nologo"
+
+mkdir_clean()
+{
+  rm -rf $1 && mkdir -p $1
+}
+
+#overwrite sign file
+SIGN_FILE=${ROOT}/sign.bat
+if [ -f ${SIGN_FILE} ]; then
+   cp ${SIGN_FILE} ${REPO}
+fi
+   
+#build and sign the installers
+. ${REPO}/mk/build-installers.sh
+
+#collect output and extra files to the OUTPUT_DIR
+EN_CD_DIR=${OUTPUT_DIR}/CD_FILES.main/client_install
+mkdir_clean ${EN_CD_DIR}
+cp ${DOTNETINST}/${BRANDING_BRAND_CONSOLE}Setup.exe ${EN_CD_DIR}
+cp ${REPO}/Branding/Images/AppIcon.ico ${EN_CD_DIR}/${BRANDING_BRAND_CONSOLE}.ico
+L10N_CD_DIR=${OUTPUT_DIR}/client_install
+mkdir_clean ${L10N_CD_DIR}
+cp ${DOTNETINST}/${BRANDING_BRAND_CONSOLE}Setup.l10n.exe ${L10N_CD_DIR}
+
+echo "INFO:	Build phase succeeded at "
+date
+
+set +u
