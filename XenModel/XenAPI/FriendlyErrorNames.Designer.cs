0e3f001578e9bff02a09aa7d50ccc8a59f92e6ee
Mihaela Stoica <mihaela.stoica@citrix.com>
2016-03-04 13:24:43 +0000

    CA-201119: Updating XenModel/XenAPI with the latest SDK
    This commit contains fixes for CA-201119, CA-201361 and CA-202268
    
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenModel/XenAPI/FriendlyErrorNames.Designer.cs
+++ XenModel/XenAPI/FriendlyErrorNames.Designer.cs
@@ -115,7 +115,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer was unable to contact your domain server to enable external authentication. Check that your settings are correct and a route to the server exists..
+        ///   Looks up a localized string similar to Unable to contact your domain server to enable external authentication. Check that your settings are correct and a route to the server exists..
         /// </summary>
         public static string AUTH_ENABLE_FAILED_DOMAIN_LOOKUP_FAILED {
             get {
@@ -642,6 +642,15 @@ namespace XenAPI {
         public static string EVENTS_LOST {
             get {
                 return ResourceManager.GetString("EVENTS_LOST", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to An emulator required to run this VM failed to start.
+        /// </summary>
+        public static string FAILED_TO_START_EMULATOR {
+            get {
+                return ResourceManager.GetString("FAILED_TO_START_EMULATOR", resourceCulture);
             }
         }
         
@@ -1384,15 +1393,6 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The host joining the pool cannot contain any network bond..
-        /// </summary>
-        public static string JOINING_HOST_CANNOT_CONTAIN_NETWORK_BOND {
-            get {
-                return ResourceManager.GetString("JOINING_HOST_CANNOT_CONTAIN_NETWORK_BOND", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to The server joining the pool cannot contain any shared storage..
         /// </summary>
         public static string JOINING_HOST_CANNOT_CONTAIN_SHARED_SRS {
@@ -1501,7 +1501,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to This type of license file is for previous versions of XenServer. Please upgrade to the new licensing system..
+        ///   Looks up a localized string similar to This type of license file is for previous versions. Please upgrade to the new licensing system..
         /// </summary>
         public static string LICENSE_FILE_DEPRECATED {
             get {
@@ -1528,7 +1528,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer was unable to contact your domain server. Check your settings are correct and that a route to the server exists..
+        ///   Looks up a localized string similar to Unable to contact your domain server. Check your settings are correct and that a route to the server exists..
         /// </summary>
         public static string LIKEWISE_WINERROR_0251E {
             get {
@@ -1861,7 +1861,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer Tools ISO must be ejected from all running VMs in the pool..
+        ///   Looks up a localized string similar to Tools ISO must be ejected from all running VMs in the pool..
         /// </summary>
         public static string PATCH_PRECHECK_FAILED_ISO_MOUNTED {
             get {
@@ -1906,7 +1906,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer build number ({1}) is incompatible with the update build number ({2})..
+        ///   Looks up a localized string similar to Current build number ({1}) is incompatible with the update build number ({2})..
         /// </summary>
         public static string PATCH_PRECHECK_FAILED_WRONG_SERVER_BUILD {
             get {
@@ -1915,7 +1915,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer version number ({1}) is incompatible with the update version number regular expression ({2})..
+        ///   Looks up a localized string similar to Current version number ({1}) is incompatible with the update version number regular expression ({2})..
         /// </summary>
         public static string PATCH_PRECHECK_FAILED_WRONG_SERVER_VERSION {
             get {
@@ -1974,6 +1974,15 @@ namespace XenAPI {
         public static string PIF_ALREADY_BONDED {
             get {
                 return ResourceManager.GetString("PIF_ALREADY_BONDED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Only one PIF on a bond is allowed to have an IP configuration..
+        /// </summary>
+        public static string PIF_BOND_MORE_THAN_ONE_IP {
+            get {
+                return ResourceManager.GetString("PIF_BOND_MORE_THAN_ONE_IP", resourceCulture);
             }
         }
         
@@ -2158,7 +2167,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to XenServer was unable to contact your domain server to enable external authentication. Check that your settings are correct and a route to the server exists..
+        ///   Looks up a localized string similar to Unable to contact your domain server to enable external authentication. Check that your settings are correct and a route to the server exists..
         /// </summary>
         public static string POOL_AUTH_ENABLE_FAILED_DOMAIN_LOOKUP_FAILED {
             get {
@@ -2239,6 +2248,15 @@ namespace XenAPI {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The host joining the pool may not have any bonds, VLANs or tunnels..
+        /// </summary>
+        public static string POOL_JOINING_HOST_MUST_ONLY_HAVE_PHYSICAL_PIFS {
+            get {
+                return ResourceManager.GetString("POOL_JOINING_HOST_MUST_ONLY_HAVE_PHYSICAL_PIFS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The provision call failed because it ran out of space..
         /// </summary>
         public static string PROVISION_FAILED_OUT_OF_SPACE {
@@ -2307,7 +2325,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The restore could not be performed because the restore script failed. Your XenServer Backup file may be corrupt. See the logs for more information..
+        ///   Looks up a localized string similar to The restore could not be performed because the restore script failed. Your Backup file may be corrupt. See the logs for more information..
         /// </summary>
         public static string RESTORE_SCRIPT_FAILED {
             get {
@@ -4134,20 +4152,20 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The SR is currently detached on the master..
-        /// </summary>
-        public static string SR_DETACHED_ON_MASTER {
-            get {
-                return ResourceManager.GetString("SR_DETACHED_ON_MASTER", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to The SR operation cannot be performed because a device underlying the SR is in use by the server..
         /// </summary>
         public static string SR_DEVICE_IN_USE {
             get {
                 return ResourceManager.GetString("SR_DEVICE_IN_USE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You attempted to migrate a VDI on SR which doesn&apos;t have snapshot capability.
+        /// </summary>
+        public static string SR_DOES_NOT_SUPPORT_MIGRATION {
+            get {
+                return ResourceManager.GetString("SR_DOES_NOT_SUPPORT_MIGRATION", resourceCulture);
             }
         }
         
@@ -4706,6 +4724,15 @@ namespace XenAPI {
         public static string VIF_IN_USE {
             get {
                 return ResourceManager.GetString("VIF_IN_USE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This VIF was not mapped to a destination Network in VM.migrate_send operation.
+        /// </summary>
+        public static string VIF_NOT_IN_MAP {
+            get {
+                return ResourceManager.GetString("VIF_NOT_IN_MAP", resourceCulture);
             }
         }
         
@@ -5322,7 +5349,7 @@ namespace XenAPI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The WLB server said something that XenServer wasn&apos;t expecting or didn&apos;t understand..
+        ///   Looks up a localized string similar to The WLB server said something that your server wasn&apos;t expecting or didn&apos;t understand..
         /// </summary>
         public static string WLB_MALFORMED_RESPONSE {
             get {
