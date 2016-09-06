7b78038a2b358bf87aad2e5ca0ecabaccaef25e8
Callum McIntyre <callumiandavid.mcintyre@citrix.com>
2016-09-06 09:48:34 +0100

    Revert "CA-149965: Snapshot name is truncated on “Revert To Snapback” dialog."
    This reverts commit 79f734f0ff6c9989d9c6bd29c62f9f68ab5b8786.

--- XenAdmin/Dialogs/RevertDialog.Designer.cs
+++ XenAdmin/Dialogs/RevertDialog.Designer.cs
@@ -32,14 +32,10 @@ namespace XenAdmin.Dialogs
             this.label1 = new System.Windows.Forms.Label();
             this.questionLabel = new System.Windows.Forms.Label();
             this.takeSnapshotBeforeCheckBox = new System.Windows.Forms.CheckBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.yesButton = new System.Windows.Forms.Button();
             this.noButton = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -61,6 +57,20 @@ namespace XenAdmin.Dialogs
             this.takeSnapshotBeforeCheckBox.Name = "takeSnapshotBeforeCheckBox";
             this.takeSnapshotBeforeCheckBox.UseVisualStyleBackColor = true;
             // 
+            // yesButton
+            // 
+            this.yesButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            resources.ApplyResources(this.yesButton, "yesButton");
+            this.yesButton.Name = "yesButton";
+            this.yesButton.UseVisualStyleBackColor = true;
+            // 
+            // noButton
+            // 
+            this.noButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.noButton, "noButton");
+            this.noButton.Name = "noButton";
+            this.noButton.UseVisualStyleBackColor = true;
+            // 
             // pictureBox1
             // 
             resources.ApplyResources(this.pictureBox1, "pictureBox1");
@@ -68,50 +78,20 @@ namespace XenAdmin.Dialogs
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
             // 
-            // tableLayoutPanel1
-            // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.questionLabel, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.takeSnapshotBeforeCheckBox, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 3);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // 
-            // yesButton
-            // 
-            resources.ApplyResources(this.yesButton, "yesButton");
-            this.yesButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.yesButton.Name = "yesButton";
-            this.yesButton.UseVisualStyleBackColor = true;
-            // 
-            // noButton
-            // 
-            resources.ApplyResources(this.noButton, "noButton");
-            this.noButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.noButton.Name = "noButton";
-            this.noButton.UseVisualStyleBackColor = true;
-            // 
-            // flowLayoutPanel1
-            // 
-            resources.ApplyResources(this.flowLayoutPanel1, "flowLayoutPanel1");
-            this.flowLayoutPanel1.Controls.Add(this.noButton);
-            this.flowLayoutPanel1.Controls.Add(this.yesButton);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            // 
             // RevertDialog
             // 
             this.AcceptButton = this.yesButton;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.CancelButton = this.noButton;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.takeSnapshotBeforeCheckBox);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.label1);
             this.Name = "RevertDialog";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,10 +102,8 @@ namespace XenAdmin.Dialogs
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label questionLabel;
         private System.Windows.Forms.CheckBox takeSnapshotBeforeCheckBox;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button noButton;
         private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
