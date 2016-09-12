xenserver/xenadmin

commit 5555709337d60c9c786d7058fb2f8100982c4b5e

[CA-209198] Allowed to attempt to install hotfix when it is already applied
XenCenter no longer ignores checking if the master host already has the specified patch applied; it now checks before trying to apply it, which means we get no error in the Patching Wizard's Patching Page.

Signed-off-by: Frezzle <frederico.mazzone@citrix.com>

XenAdmin/Wizards/PatchingWizard/PatchingWizard_PatchingPage.cs
@@ -171,7 +171,7 @@ namespace XenAdmin.Wizards.PatchingWizard
 
                 List<Host> poolHosts = new List<Host>(pool.Connection.Cache.Hosts);
                 Host master = SelectedServers.Find(host => host.IsMaster() && poolHosts.Contains(host));
-                if (master != null)
+                if (master != null && poolPatch != null && poolPatch.AppliedOn(master) == DateTime.MaxValue)
                     planActions.AddRange(CompileActionList(master, poolPatch));
                 foreach (Host server in SelectedServers)
                 {

XenModel/Actions/Pool_Patch/ApplyPatchAction.cs
@@ -130,7 +130,8 @@ namespace XenAdmin.Actions
             {
                 foreach (Host host in hosts)
                 {
-                    ApplyPatch(host, patch);
+                    if (patch.AppliedOn(host) == DateTime.MaxValue)
+                        ApplyPatch(host, patch);
                 }
             }
         

commit fdb32cf5798482e0887c883504e4a4f024f16562

CA-220599: Supplemental Pack installed via XenCenter does not get listed in updates box
Fixed regression caused by 5555709337d60c9c786d7058fb2f8100982c4b5e.
Supplemental packs can now be installed again in automatic mode.

Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>

XenAdmin/Wizards/PatchingWizard/PatchingWizard_PatchingPage.cs
@@ -179,7 +179,7 @@ namespace XenAdmin.Wizards.PatchingWizard
 
                 List<Host> poolHosts = new List<Host>(pool.Connection.Cache.Hosts);
                 Host master = SelectedServers.Find(host => host.IsMaster() && poolHosts.Contains(host));
-                if (master != null && poolPatch != null && poolPatch.AppliedOn(master) == DateTime.MaxValue)
+                if (master != null && (poolPatch == null || poolPatch.AppliedOn(master) == DateTime.MaxValue))
                     planActions.AddRange(CompileActionList(master, poolPatch));
                 foreach (Host server in SelectedServers)
                 {