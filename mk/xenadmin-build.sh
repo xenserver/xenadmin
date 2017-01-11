71547b90db69da6b956fb88756804ca96867c11b
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2017-01-11 15:57:28 +0000

    CP-20149: do not have the build scripts copy the production output to a network share.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- mk/xenadmin-build.sh
+++ mk/xenadmin-build.sh
@@ -50,7 +50,6 @@ mkdir_clean()
 #clear all working directories before anything else
 mkdir_clean ${SCRATCH_DIR}
 mkdir_clean ${OUTPUT_DIR}
-mkdir_clean ${BUILD_ARCHIVE}
 rm -rf ${TEST_DIR}/* ${XENCENTER_LOGDIR}/*.log || true
 
 #create manifest
@@ -81,12 +80,6 @@ if [ -d ${ROOT}/xenadmin-branding.git/${XC_BRANDING} ]; then
     cp -rf ${ROOT}/xenadmin-branding.git/${XC_BRANDING}/* ${XENADMIN_DIR}/Branding/
 fi
 
-if [ "${BUILD_KIND:+$BUILD_KIND}" = production ]
-then
-    ( mkdir -p ${BUILD_TOOLS%/*} && cd ${BUILD_TOOLS%/*} && git clone ${BUILD_TOOLS_REPO} ${BUILD_TOOLS##*/} )
-    chmod +x ${BUILD_TOOLS}/scripts/storefiles.py
-fi
-
 WIX_INSTALLER_DEFAULT_GUID=65AE1345-A520-456D-8A19-2F52D43D3A09
 WIX_INSTALLER_DEFAULT_GUID_VNCCONTROL=0CE5C3E7-E786-467a-80CF-F3EC04D414E4
 WIX_INSTALLER_DEFAULT_VERSION=1.0.0
@@ -110,42 +103,19 @@ mkdir_clean ${DISCUTILS_DIR}
 mkdir_clean ${MICROSOFT_DOTNET_FRAMEWORK_INSTALLER_DIR}
 mkdir_clean ${PUTTY_DIR}
 
-dotnet_cp_to_dir ()
-{
-    local -r destdir="${1}"; shift
-    local -r src="${1}"; shift
-    if [ "${BUILD_KIND:+$BUILD_KIND}" = production ]
-    then
-        cp "${DOTNET_LOC}/${src}" "${destdir}/" || cp "${DOTNET_LOC_TRUNK}/${src}" "${destdir}/"
-    else
-        WGET -P "${destdir}" "${WEB_DOTNET}/${src}"
-    fi
-}
-
-dotnet_cp_file ()
-{
-    local -r src="${1}"; shift
-    local -r dest="${1}"; shift
-    if [ "${BUILD_KIND:+$BUILD_KIND}" = production ]
-    then
-        cp "${DOTNET_LOC}/${src}" "${dest}" || cp "${DOTNET_LOC_TRUNK}/${src}" "${dest}"
-    else
-        WGET -O "${dest}" "${WEB_DOTNET}/${src}"
-    fi
-}
-
-dotnet_cp_file "manifest" "${SCRATCH_DIR}/dotnet-packages-manifest"
-dotnet_cp_to_dir "${XMLRPC_DIR}" "UNSIGNED/CookComputing.XmlRpcV2.dll"
-dotnet_cp_to_dir "${LOG4NET_DIR}" "UNSIGNED/log4net.dll"
-dotnet_cp_to_dir "${SHARPZIPLIB_DIR}" "UNSIGNED/ICSharpCode.SharpZipLib.dll"
-dotnet_cp_to_dir "${DOTNETZIP_DIR}" "UNSIGNED/Ionic.Zip.dll"
-dotnet_cp_to_dir "${DISCUTILS_DIR}" "UNSIGNED/DiscUtils.dll"
-dotnet_cp_to_dir "${MICROSOFT_DOTNET_FRAMEWORK_INSTALLER_DIR}" "NDP46-KB3045560-Web.exe"
-dotnet_cp_to_dir "${PUTTY_DIR}" "UNSIGNED/putty.exe"
-dotnet_cp_to_dir "${REPO}" "sign.bat" && chmod a+x "${REPO}/sign.bat"
+WGET -O ${SCRATCH_DIR}/dotnet-packages-manifest ${WEB_DOTNET}/manifest
+WGET -P ${XMLRPC_DIR}      ${WEB_DOTNET}/UNSIGNED/CookComputing.XmlRpcV2.dll
+WGET -P ${LOG4NET_DIR}     ${WEB_DOTNET}/UNSIGNED/log4net.dll
+WGET -P ${SHARPZIPLIB_DIR} ${WEB_DOTNET}/UNSIGNED/ICSharpCode.SharpZipLib.dll
+WGET -P ${DOTNETZIP_DIR}   ${WEB_DOTNET}/UNSIGNED/Ionic.Zip.dll
+WGET -P ${DISCUTILS_DIR}   ${WEB_DOTNET}/UNSIGNED/DiscUtils.dll
+WGET -P ${MICROSOFT_DOTNET_FRAMEWORK_INSTALLER_DIR} ${WEB_DOTNET}/NDP46-KB3045560-Web.exe
+WGET -P ${PUTTY_DIR}       ${WEB_DOTNET}/UNSIGNED/putty.exe
+WGET -P ${REPO}            ${WEB_DOTNET}/sign.bat
+chmod a+x ${REPO}/sign.bat
 
 #bring in the ovf fixup iso from artifactory (currently one location)
-WGET -P "${SCRATCH_DIR}" ${REPO_CITRITE_HOST}/list/xs-local-contrib/citrix/xencenter/XenCenterOVF.zip
+WGET -P ${SCRATCH_DIR} ${REPO_CITRITE_HOST}/list/xs-local-contrib/citrix/xencenter/XenCenterOVF.zip
 ${UNZIP} -d ${REPO}/XenOvfApi ${SCRATCH_DIR}/XenCenterOVF.zip
 
 #bring in some more libraries
@@ -293,7 +263,6 @@ cp ${REPO}/XenAdmin/bin/Release/{CommandLib.pdb,${BRANDING_BRAND_CONSOLE}.pdb,Xe
    ${REPO}/XenServerHealthCheck/bin/Release/XenServerHealthCheck.pdb \
    ${OUTPUT_DIR}
 
-echo "INFO:	Create English iso files"
 ISO_DIR=${SCRATCH_DIR}/iso-staging
 mkdir_clean ${ISO_DIR}
 install -m 755 ${DOTNETINST}/${BRANDING_BRAND_CONSOLE}Setup.exe ${ISO_DIR}/${BRANDING_BRAND_CONSOLE}Setup.exe
@@ -304,7 +273,6 @@ mkdir_clean ${OUTPUT_DIR}/installer
 tar cjf ${OUTPUT_DIR}/installer/${BRANDING_BRAND_CONSOLE}.installer.tar.bz2 -C ${ISO_DIR} .
 install -m 755 ${DOTNETINST}/${BRANDING_BRAND_CONSOLE}Setup.exe ${OUTPUT_DIR}/installer/${BRANDING_BRAND_CONSOLE}Setup.exe
 
-echo "INFO:	Create l10n iso file"
 L10N_ISO_DIR=${SCRATCH_DIR}/l10n-iso-staging
 mkdir_clean ${L10N_ISO_DIR}
 # -o root -g root 
@@ -319,7 +287,7 @@ install -m 755 ${DOTNETINST}/${BRANDING_BRAND_CONSOLE}Setup.l10n.exe ${OUTPUT_DI
 #bring in the pdbs from dotnet-packages latest build
 for pdb in CookComputing.XmlRpcV2.pdb DiscUtils.pdb ICSharpCode.SharpZipLib.pdb Ionic.Zip.pdb log4net.pdb
 do
-  dotnet_cp_to_dir "${OUTPUT_DIR}" "${pdb}"
+  WGET -P ${OUTPUT_DIR} ${WEB_DOTNET}/${pdb}
 done
 
 #now package the pdbs
@@ -330,11 +298,6 @@ echo "xencenter-ovf xencenter-ovf.git 21d3d7a7041f15abfa73f916e5fd596fd7e610c4" 
 echo "chroot-lenny chroots.hg 1a75fa5848e8" >> ${OUTPUT_DIR}/manifest
 
 cat ${SCRATCH_DIR}/dotnet-packages-manifest >> ${OUTPUT_DIR}/manifest
-get_BUILD_PATH=/usr/groups/xen/carbon/windowsbuilds/WindowsBuilds/${get_JOB_NAME}/${BUILD_NUMBER}
-if [ "${BUILD_KIND:+$BUILD_KIND}" = production ]
-then
-    echo ${get_BUILD_URL} > ${OUTPUT_DIR}/latest-secure-build
-fi
 
 echo "INFO:	Build phase succeeded at "
 date
