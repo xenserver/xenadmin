a95dc00f8e57e6ea78474af1e1c5399f28b789d8
Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
2017-03-17 10:57:43 +0000

    CP-21178: Extend metadata on XenServerVersion
    Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>

--- XenModel/Actions/Updates/DownloadUpdatesXmlAction.cs
+++ XenModel/Actions/Updates/DownloadUpdatesXmlAction.cs
@@ -237,6 +237,7 @@ namespace XenAdmin.Actions
                     string url = "";
                     string timestamp = "";
                     string buildNumber = "";
+                    string patchUuid = "";
 
                     foreach (XmlAttribute attrib in version.Attributes)
                     {
@@ -254,6 +255,8 @@ namespace XenAdmin.Actions
                             timestamp = attrib.Value;
                         else if (attrib.Name == "build-number")
                             buildNumber = attrib.Value;
+                        else if (attrib.Name == "patch-uuid")
+                            patchUuid = attrib.Value;
                     }
 
                     List<XenServerPatch> patches = new List<XenServerPatch>();
@@ -289,7 +292,7 @@ namespace XenAdmin.Actions
                     }
 
                     XenServerVersions.Add(new XenServerVersion(version_oem, name, is_latest, is_latest_cr, url, patches, minimalPatches, timestamp,
-                                                               buildNumber));
+                                                               buildNumber, patchUuid));
                 }
             }
         }
