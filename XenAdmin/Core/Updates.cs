66303bc07355ee00ef0114e68a74a6e83022087f
Mihaela Stoica <mihaela.stoica@citrix.com>
2017-03-20 10:17:35 +0000

    CP-21179: Show an update alert as a new XenServer version
    If a patch updates the server version, then save a reference to that version in the alert and display this alert as a "new version" alert
    
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/Core/Updates.cs
+++ XenAdmin/Core/Updates.cs
@@ -405,6 +405,8 @@ namespace XenAdmin.Core
 
             var alerts = new List<XenServerPatchAlert>();
 
+            var xenServerVersionsAsUpdates = xenServerVersions.Where(v => v.IsVersionAvailableAsAnUpdate);
+
             foreach (IXenConnection xenConnection in ConnectionsManager.XenConnectionsCopy)
             {
                 Host master = Helpers.GetMaster(xenConnection);
@@ -436,7 +438,9 @@ namespace XenAdmin.Core
 
                     foreach (XenServerPatch xenServerPatch in patches)
                     {
-                        var alert = new XenServerPatchAlert(xenServerPatch);
+                        XenServerVersion newServerVersion = xenServerVersionsAsUpdates.FirstOrDefault(newVersion => newVersion.PatchUuid.Equals(xenServerPatch.Uuid, StringComparison.OrdinalIgnoreCase));
+
+                        var alert = new XenServerPatchAlert(xenServerPatch, newServerVersion);
                         var existingAlert = alerts.Find(al => al.Equals(alert));
 
                         if (existingAlert != null)
