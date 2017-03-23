88e090c66c8bdabfdce78fa126c97c4b5c85aa1e
Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
2017-03-23 13:58:24 +0000

    CP-21500: Updates wizard: Show new versions that are available as updates
    In the Updates wizard:
    New versions that are available as updates will be listed in the "Download Updates from Citrix" section, before any other updates.
    If XenCenter is not the latest version, these rows will be greyed out, with a tooltip.
    
    Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>

---
+++ XenAdmin/Alerts/NewVersionPriorityAlertComparer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XenAdmin.Alerts
+{
+    public class NewVersionPriorityAlertComparer : IComparer<Alert>
+    {
+        public int Compare(Alert alert1, Alert alert2)
+        {
+            if (alert1 == null || alert2 == null)
+                return 0;
+
+            int sortResult = 0;
+
+            if (IsVersionOrVersionUpdateAlert(alert1) && !IsVersionOrVersionUpdateAlert(alert2))
+                sortResult = 1;
+
+            if (!IsVersionOrVersionUpdateAlert(alert1) && IsVersionOrVersionUpdateAlert(alert2))
+                sortResult = -1;
+
+            if (sortResult == 0)
+                sortResult = Alert.CompareOnDate(alert1, alert2);
+
+            return -sortResult;
+        }
+
+        private bool IsVersionOrVersionUpdateAlert(Alert alert)
+        {
+            return alert is XenServerPatchAlert && (alert as XenServerPatchAlert).NewServerVersion != null
+                || alert is XenServerVersionAlert
+                || alert is XenCenterUpdateAlert;
+        }
+    }
+}
--- XenAdmin/TabPages/ManageUpdatesPage.cs
+++ XenAdmin/TabPages/ManageUpdatesPage.cs
@@ -1009,33 +1009,6 @@ namespace XenAdmin.TabPages
             labelProgress.MaximumSize = new Size(tableLayoutPanel3.Width - 60, tableLayoutPanel3.Size.Height);
         }
 
-        public class NewVersionPriorityAlertComparer : IComparer<Alert>
-        {
-            public int Compare(Alert alert1, Alert alert2)
-            {
-                if (alert1 == null || alert2 == null)
-                    return 0;
 
-                int sortResult = 0;
-
-                if (IsVersionOrVersionUpdateAlert(alert1) && !IsVersionOrVersionUpdateAlert(alert2))
-                    sortResult = 1;
-
-                if (!IsVersionOrVersionUpdateAlert(alert1) && IsVersionOrVersionUpdateAlert(alert2))
-                    sortResult = -1;
-
-                if (sortResult == 0)
-                    sortResult = Alert.CompareOnDate(alert1, alert2);
-
-                return -sortResult;
-            }
-
-            private bool IsVersionOrVersionUpdateAlert(Alert alert)
-            {
-                return alert is XenServerPatchAlert && (alert as XenServerPatchAlert).NewServerVersion != null
-                    || alert is XenServerVersionAlert
-                    || alert is XenCenterUpdateAlert;
-            }
-        }
     }
 }
