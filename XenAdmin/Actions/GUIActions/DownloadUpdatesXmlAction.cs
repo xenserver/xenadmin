385d0bccd5594a8cad7618026aac0c46dd6f3af4
Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
2017-04-11 10:53:34 +0100

    CA-249056: Add a unique ID to XenCenter Check For Update
    Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
    
    Conflicts:
    	XenAdmin/Actions/GUIActions/DownloadUpdatesXmlAction.cs
    
    a

--- XenAdmin/Actions/GUIActions/DownloadUpdatesXmlAction.cs
+++ XenAdmin/Actions/GUIActions/DownloadUpdatesXmlAction.cs
@@ -38,6 +38,7 @@ using System.Xml;
 using XenAdmin.Core;
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 
 
 namespace XenAdmin.Actions
@@ -302,6 +303,12 @@ namespace XenAdmin.Actions
             var xdoc = new XmlDocument();
             var uri = new Uri(location);
             var proxy = XenAdminConfigManager.Provider.GetProxyFromSettings(Connection);
+            
+            var uniqueIdHash = GetUniqueIdHash();
+
+            string buildText = Helpers.CommonCriteriaCertificationRelease
+                        ? string.Format("{0}: {1}", Program.Version.Revision, Messages.COMMON_CRITERIA_TEXT)
+                        : Program.Version.Revision.ToString();
 
             if (uri.IsFile)
             {
@@ -312,6 +319,8 @@ namespace XenAdmin.Actions
                 using (var webClient = new WebClient())
                 {
                     webClient.Proxy = proxy;
+                    webClient.Headers.Add("User-Agent", string.Format("{0} (v.{1})", Branding.BRAND_CONSOLE , buildText));
+                    webClient.Headers.Add("User-Agent-Id", uniqueIdHash);
 
                     using (var stream = new MemoryStream(webClient.DownloadData(uri)))
                     {
@@ -321,6 +330,27 @@ namespace XenAdmin.Actions
             }
 
             return xdoc;
+        }
+
+        private string GetUniqueIdHash()
+        {
+            string uniqueIdHash = "nil";
+
+            var managementObj = new System.Management.ManagementObject("Win32_OperatingSystem=@");
+            string serialNumber = (string)managementObj["SerialNumber"];
+
+            if (!string.IsNullOrWhiteSpace(serialNumber))
+            {
+                var serialBytes = Encoding.ASCII.GetBytes("JustSomeSaltForCheckForUpdates" + serialNumber);
+
+                using (var md = new System.Security.Cryptography.MD5CryptoServiceProvider()) // MD5 to keep it short enough as this hash is not used for security in any way
+                {
+                    var hash = md.ComputeHash(serialBytes);
+                    uniqueIdHash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+
+            return uniqueIdHash;
         }
     }
 }
