7ccdad17ded72e61754b3906b571ddbd3f5e27fb
Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>
2017-04-25 17:35:36 +0100

    CA-249056: Add a unique ID to XenCenter Check For Update
    Changes following the code review
    
    Signed-off-by: Gabor Apati-Nagy <gabor.apati-nagy@citrix.com>

--- XenModel/Actions/Updates/DownloadUpdatesXmlAction.cs
+++ XenModel/Actions/Updates/DownloadUpdatesXmlAction.cs
@@ -310,7 +310,7 @@ namespace XenAdmin.Actions
                 {
                     webClient.Proxy = proxy;
                     webClient.Headers.Add("User-Agent", _userAgent);
-                    webClient.Headers.Add("User-Agent-Id", _userAgentId);
+                    webClient.Headers.Add("X-User-Agent-Id", _userAgentId);
 
                     using (var stream = new MemoryStream(webClient.DownloadData(uri)))
                     {
