a17063ec7d20310b7d574ef61e92ed19ea55b84f
Letsibogo Ramadi <letsibogo.ramadi@citrix.com>
2017-06-20 12:29:03 +0100

    CA-177402 - XenCenter doesnt show migrate option on selecting multiple Suspended VMs
    Signed-off-by: Letsibogo Ramadi <letsibogo.ramadi@citrix.com>

--- XenAdmin/Commands/MoveVMCommand.cs
+++ XenAdmin/Commands/MoveVMCommand.cs
@@ -67,7 +67,7 @@ namespace XenAdmin.Commands
         protected override void ExecuteCore(SelectedItemCollection selection)
         {
 
-            if (selection.AllItemsAre<VM>() && new CrossPoolMoveVMCommand(MainWindowCommandInterface, selection).CanExecute())
+            if (selection.Count > 1 && new CrossPoolMoveVMCommand(MainWindowCommandInterface, selection).CanExecute())
             {
                 new CrossPoolMoveVMCommand(MainWindowCommandInterface, selection).Execute();
             }
