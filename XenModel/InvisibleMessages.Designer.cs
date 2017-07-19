b0f5b8ebc4ea472e8e2ac79f9ebe368724b43278
Letsibogo Ramadi <letsibogo.ramadi@citrix.com>
2017-07-19 19:07:42 +0100

    CA-258021 - Change updates retrieval url (#1702)
    * CA-258021 - Change updates retrieval url
    
    Signed-off-by: Letsibogo Ramadi <letsibogo.ramadi@citrix.com>
    
    * CA-258021 - Change updates retrieval url
    
    - Update url in Branding.sh
    
    Signed-off-by: Letsibogo Ramadi <letsibogo.ramadi@citrix.com>

--- XenModel/InvisibleMessages.Designer.cs
+++ XenModel/InvisibleMessages.Designer.cs
@@ -439,7 +439,7 @@ namespace XenAdmin {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://updates.xensource.com/XenServer/updates.xml.
+        ///   Looks up a localized string similar to https://updates.xensource.com/XenServer/updates.xml.
         /// </summary>
         public static string XENSERVER_UPDATE_URL {
             get {
