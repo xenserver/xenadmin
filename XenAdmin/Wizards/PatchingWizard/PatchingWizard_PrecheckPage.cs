dc2968b3d4144c9033b19488cac714b4f11f3b3c
Mihaela Stoica <mihaela.stoica@citrix.com>
2017-09-01 15:34:58 +0100

    CP-24215: Add null checks when we log the minimal patches
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenAdmin/Wizards/PatchingWizard/PatchingWizard_PrecheckPage.cs
+++ XenAdmin/Wizards/PatchingWizard/PatchingWizard_PrecheckPage.cs
@@ -400,7 +400,7 @@ namespace XenAdmin.Wizards.PatchingWizard
                         ? Updates.GetUpgradeSequence(pool.Connection, UpdateAlert, ApplyUpdatesToNewVersion && !automatedUpdatesRestricted)
                         : Updates.GetUpgradeSequence(pool.Connection);
 
-                    log.InfoFormat("Minimal patches for {0}: {1}", pool.Name, string.Join(",", us.MinimalPatches.Select(p => p.Name)));
+                    log.InfoFormat("Minimal patches for {0}: {1}", pool.Name, us != null && us.MinimalPatches != null ? string.Join(",", us.MinimalPatches.Select(p => p.Name)) : "");
 
                     if (us == null)
                         continue;
