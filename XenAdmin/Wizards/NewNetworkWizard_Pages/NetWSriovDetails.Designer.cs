b2c74ff2610a353c5084ad7c7f559426f4f1dd18
jishengx <jisheng.xing@citrix.com>
2018-01-05 20:46:44 +0800

    CP-25982: add wizard to enable SR-IOV network (#1887)

---
+++ XenAdmin/Wizards/NewNetworkWizard_Pages/NetWSriovDetails.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace XenAdmin.Wizards.NewNetworkWizard_Pages
+{
+    partial class NetWSriovDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NetWSriovDetails));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.cbxAutomatic = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxNicList = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.cbxAutomatic, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxNicList, 1, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // cbxAutomatic
+            // 
+            resources.ApplyResources(this.cbxAutomatic, "cbxAutomatic");
+            this.tableLayoutPanel1.SetColumnSpan(this.cbxAutomatic, 3);
+            this.cbxAutomatic.Name = "cbxAutomatic";
+            this.cbxAutomatic.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 3);
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBoxNicList
+            // 
+            this.comboBoxNicList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxNicList.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBoxNicList, "comboBoxNicList");
+            this.comboBoxNicList.Name = "comboBoxNicList";
+            this.comboBoxNicList.Sorted = true;
+            // 
+            // NetWSriovDetails
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "NetWSriovDetails";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxNicList;
+        private System.Windows.Forms.CheckBox cbxAutomatic;
+    }
+}
---
+++ XenAdmin/Wizards/NewNetworkWizard_Pages/NetWSriovDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using XenAdmin.Controls;
+using XenAPI;
+using XenAdmin.Network;
+
+namespace XenAdmin.Wizards.NewNetworkWizard_Pages
+{
+    public partial class NetWSriovDetails : XenTabPage
+    {
+        internal Host Host;
+
+        public NetWSriovDetails()
+        {
+            InitializeComponent();
+        }
+        public override string Text { get { return Messages.NETW_DETAILS_TEXT; } }
+
+        public override string PageTitle
+        {
+            get
+            {
+                return Messages.NETW_INTERNAL_DETAILS_TITLE;
+            }
+        }
+        public override void PopulatePage()
+        {
+            PopulateHostNicList(Host, Connection);
+        }
+        private void PopulateHostNicList(Host host, IXenConnection conn)
+        {
+            comboBoxNicList.Items.Clear();
+            foreach (PIF thePIF in conn.Cache.PIFs)
+            {
+                if (thePIF.host.opaque_ref == host.opaque_ref && thePIF.IsPhysical() && !thePIF.IsBondNIC())
+                {
+                    comboBoxNicList.Items.Add(thePIF);
+                }
+            }
+            if (comboBoxNicList.Items.Count > 0)
+                comboBoxNicList.SelectedIndex = 0;
+        }
+    }
+}
