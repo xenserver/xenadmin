b0fdf65fc904d0454c3ff5e7ba35c733a8a4c3a5
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2018-01-10 13:01:00 +0000

    CP-26469: Added version 2.9 to the API bindings.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenModel/XenAPI/ApiVersion.cs
+++ XenModel/XenAPI/ApiVersion.cs
@@ -56,7 +56,8 @@ namespace XenAPI
         API_2_6 = 17, //XenServer 7.1 (ely)
         API_2_7 = 18, //XenServer 7.2 (falcon)
         API_2_8 = 19, //XenServer 7.3 (inverness)
-        LATEST = 19,
+        API_2_9 = 20, //XenServer 7.4 (jura)
+        LATEST = 20,
         UNKNOWN = 99
     }
 
@@ -104,6 +105,8 @@ namespace XenAPI
                     return "2.7";
                 case API_Version.API_2_8:
                     return "2.8";
+                case API_Version.API_2_9:
+                    return "2.9";
                 default:
                     return "Unknown";
             }
