4346a5ff31edbba918a221d3275f5501611d712d
Jisheng Xing <jisheng.xing@citrix.com>
2018-02-24 15:59:52 +0800

    CP-26421: Remove SR-IOV network on host's Networking TabPage
    Signed-off-by: Jisheng Xing <jisheng.xing@citrix.com>

--- XenModel/Actions/Network/NetworkAction.cs
+++ XenModel/Actions/Network/NetworkAction.cs
@@ -206,7 +206,15 @@ namespace XenAdmin.Actions
                 {
                     if (!pif.physical)
                     {
-                        VLAN.destroy(Session, pif.VLAN_master_of);
+                        if(pif.VLAN != -1)
+                        {
+                            VLAN.destroy(Session, pif.VLAN_master_of);
+                        }
+                        
+                        if(pif.sriov_logical_PIF_of.Count != 0)
+                        {
+                            Network_sriov.destroy(Session, pif.sriov_logical_PIF_of[0]);
+                        }
                     }
                     else
                     {
