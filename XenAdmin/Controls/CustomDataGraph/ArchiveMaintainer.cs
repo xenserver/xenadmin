xenserver/xenadmin

commit b4956da8f1a1e7d1cef28163a81b9223cd932259

Performance and workflow improvement: - no need to register the XenObjects events with the ArchiveMaintainer since what the handler does is not specific to this class; this can be done on the Performance page where the events are also registered. - set directly the ArchiveMaintainer's XenObject to the new value without setting it intermediately to null as this causes a full data download event if the object has not changed.
Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

XenAdmin/Controls/CustomDataGraph/ArchiveMaintainer.cs
@@ -30,9 +30,7 @@
  */
 
 using System;
-using System.ComponentModel;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Net;
@@ -130,40 +128,15 @@ namespace XenAdmin.Controls.CustomDataGraph
         /// </summary>
         public IXenObject XenObject
         {
-            get { return _xenObject; }
+            private get { return _xenObject; }
             set
             {
                 Program.AssertOnEventThread();
 
-                DeregEvents();
-
-                string oldref = XenObject == null ? "" : XenObject.opaque_ref;
+                string oldref = _xenObject == null ? "" : _xenObject.opaque_ref;
                 _xenObject = value;
-                string newref = XenObject == null ? "" : XenObject.opaque_ref;
+                string newref = _xenObject == null ? "" : _xenObject.opaque_ref;
                 FirstTime = FirstTime || newref != oldref;
-
-                if (FirstTime)
-                {
-                    // Restrict to at most 24 hours data if necessary
-                    if (Helpers.FeatureForbidden(XenObject, XenAPI.Host.RestrictPerformanceGraphs))
-                    {
-                        Archives[ArchiveInterval.OneHour].MaxPoints = 24;
-                        Archives[ArchiveInterval.OneDay].MaxPoints = 0;
-                    }
-                    else
-                    {
-                        Archives[ArchiveInterval.OneHour].MaxPoints = HoursInOneWeek;
-                        Archives[ArchiveInterval.OneDay].MaxPoints = DaysInOneYear;
-                    }
-
-                    foreach (DataArchive a in Archives.Values)
-                        a.Sets.Clear();
-                    
-                    LoadingInitialData = true;
-                    OnArchivesUpdated();
-                }
-
-                RegEvents();
             }
         }
 
@@ -180,7 +153,7 @@ namespace XenAdmin.Controls.CustomDataGraph
         {
             get
             {
-                IXenObject m = _xenObject;
+                IXenObject m = XenObject;
                 return m == null ? TimeSpan.Zero : m.Connection.ServerTimeOffset;
             }
         }
@@ -193,26 +166,39 @@ namespace XenAdmin.Controls.CustomDataGraph
             Archives.Add(ArchiveInterval.OneDay, new DataArchive(DaysInOneYear));
             Archives.Add(ArchiveInterval.None, new DataArchive(0));
 
-            UpdaterThread = new Thread(new ThreadStart(Update));
-            UpdaterThread.Name = "Archive Maintainer";
-            UpdaterThread.IsBackground = true;
+            UpdaterThread = new Thread(Update) {Name = "Archive Maintainer", IsBackground = true};
         }
 
         /// <summary>
         /// Call me, async update graph data set
         /// UpdaterThread Thread
         /// </summary>
-        public void Update()
+        private void Update()
         {
             while (RunThread)
             {
                 IXenObject xenObject = XenObject;
                 Host Host = GetHost(xenObject);
 
-                DateTime ServerWas = ServerNow(); // get time before updating so we dont miss any 5 second updates if getting the past data
+                DateTime ServerWas = ServerNow(); // get time before updating so we don't miss any 5 second updates if getting the past data
 
                 if (FirstTime)
                 {
+                    // Restrict to at most 24 hours data if necessary
+                    if (Helpers.FeatureForbidden(_xenObject, XenAPI.Host.RestrictPerformanceGraphs))
+                    {
+                        Archives[ArchiveInterval.OneHour].MaxPoints = 24;
+                        Archives[ArchiveInterval.OneDay].MaxPoints = 0;
+                    }
+                    else
+                    {
+                        Archives[ArchiveInterval.OneHour].MaxPoints = HoursInOneWeek;
+                        Archives[ArchiveInterval.OneDay].MaxPoints = DaysInOneYear;
+                    }
+
+                    foreach (DataArchive a in Archives.Values)
+                        a.Sets.Clear();
+
                     LoadingInitialData = true;
                     OnArchivesUpdated();
                     Get(ArchiveInterval.None, RrdsUri, RRD_Full_InspectCurrentNode, Host, xenObject);
@@ -634,60 +620,6 @@ namespace XenAdmin.Controls.CustomDataGraph
         {
             if (ArchivesUpdated != null)
                 ArchivesUpdated();
-        }
-
-        /// <summary>
-        /// Gui Thread
-        /// </summary>
-        internal void DeregEvents()
-        {
-            if (XenObject == null)
-                return;
-            Pool pool = Helpers.GetPoolOfOne(XenObject.Connection);
-            if (pool != null)
-                pool.PropertyChanged -= pool_PropertyChanged;
-
-        }
-
-        /// <summary>
-        /// Gui Thread
-        /// </summary>
-        private void RegEvents()
-        {
-            if (XenObject == null)
-                return;
-            Pool pool = Helpers.GetPoolOfOne(XenObject.Connection);
-            if (pool != null)
-                pool.PropertyChanged += pool_PropertyChanged;
-        }
-
-        /// <summary>
-        /// Random Thread
-        /// </summary>
-        void pool_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "gui_config")
-            {
-                Dictionary<string, string> gui_config = Helpers.GetGuiConfig((IXenObject)sender);
-                string uuid = Helpers.GetUuid(XenObject);
-
-                foreach (string key in gui_config.Keys)
-                {
-                    if (!Palette.OtherConfigUUIDRegex.IsMatch(key) || !key.Contains(uuid))
-                        continue;
-
-                    string value = gui_config[key];
-                    int argb;
-                    if (!Int32.TryParse(value, out argb))
-                        continue;
-
-                    string[] strs = key.Split('.');
-
-                    // just set the color, we dont care what it is
-                    Palette.SetCustomColor(Palette.GetUuid(strs[strs.Length - 1], XenObject), Color.FromArgb(argb));
-                }
-                OnArchivesUpdated();
-            }
         }
     }
 }

XenAdmin/TabPages/PerformancePage.cs
@@ -164,27 +164,28 @@ namespace XenAdmin.TabPages
 
         public IXenObject XenObject
         {
-            get
+            private get
             {
                 return _xenObject;
             }
             set
             {
                 ArchiveMaintainer.Pause();
-                ArchiveMaintainer.XenObject = null;
                 DataEventList.Clear();
 
                 DeregEvents();
                 _xenObject = value;
                 RegEvents();
 
+                ArchiveMaintainer.XenObject = value;
+
                 if (_xenObject != null)
                 {
                     GraphList.LoadGraphs(XenObject);
                     LoadEvents();
-                    ArchiveMaintainer.XenObject = value;
                     ArchiveMaintainer.Start(); 
                 }
+                RefreshAll();
             }
         }
 
@@ -259,7 +260,32 @@ namespace XenAdmin.TabPages
         private void pool_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName == "gui_config")
-                Program.Invoke(this, () => GraphList.LoadGraphs(XenObject));
+            {
+                Dictionary<string, string> gui_config = Helpers.GetGuiConfig((IXenObject)sender);
+                string uuid = Helpers.GetUuid(XenObject);
+
+                foreach (string key in gui_config.Keys)
+                {
+                    if (!Palette.OtherConfigUUIDRegex.IsMatch(key) || !key.Contains(uuid))
+                        continue;
+
+                    string value = gui_config[key];
+                    int argb;
+                    if (!Int32.TryParse(value, out argb))
+                        continue;
+
+                    string[] strs = key.Split('.');
+
+                    // just set the color, we dont care what it is
+                    Palette.SetCustomColor(Palette.GetUuid(strs[strs.Length - 1], XenObject), Color.FromArgb(argb));
+                }
+
+                Program.Invoke(this, () =>
+                {
+                    GraphList.LoadGraphs(XenObject);
+                    RefreshAll();
+                });
+            }
         }
 
         private void DeregEvents()
@@ -278,13 +304,11 @@ namespace XenAdmin.TabPages
         public override void PageHidden()
         {
             DeregEvents();
-            if (ArchiveMaintainer != null && XenObject != null)
-            {
+
+            if (ArchiveMaintainer != null)
                 ArchiveMaintainer.Pause();
-                ArchiveMaintainer.DeregEvents();
-            }
         }
-        
+
         private void ArchiveMaintainer_ArchivesUpdated()
         {
             Program.Invoke(this, RefreshAll);

commit cd8aef8a68d8d8aebf2c53eb23840dbd0825011d

CA-287714: Fixed regression introduced with b4956da8f1a1e7d1cef28163a81b9223cd932259: The DataArchive.Sets are supposed to be updated on the UI thread. Added an extra safeguard when updating the DataKey items, namely copy the archives to a local variable. Also, some minor refactoring to make looking up sets into the archives more efficient.
Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

XenAdmin/Controls/CustomDataGraph/ArchiveMaintainer.cs
@@ -197,7 +197,7 @@ namespace XenAdmin.Controls.CustomDataGraph
                     }
 
                     foreach (DataArchive a in Archives.Values)
-                        a.Sets.Clear();
+                        a.ClearSets();
 
                     LoadingInitialData = true;
                     OnArchivesUpdated();

XenAdmin/Controls/CustomDataGraph/DataArchive.cs
@@ -123,5 +123,10 @@ namespace XenAdmin.Controls.CustomDataGraph
                 });
             }
         }
+
+        internal void ClearSets()
+        {
+            Program.Invoke(Program.MainWindow, () => Sets.Clear());
+        }
     }
 }

XenAdmin/Controls/CustomDataGraph/DataKey.cs
@@ -32,6 +32,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using XenAdmin.Core;
@@ -108,27 +109,27 @@ namespace XenAdmin.Controls.CustomDataGraph
             if (DataPlot == null || DataPlot.DataPlotNav == null)
                 return;
 
-            List<DataSetCollectionWrapper> wrappers = new List<DataSetCollectionWrapper>();
+            var wrappers = new List<DataSetCollectionWrapper>();
+            var archives = ArchiveMaintainer.Archives;
+            var fiveSecSets = archives[ArchiveInterval.FiveSecond].Sets;
 
-            for (int i = 0; i < ArchiveMaintainer.Archives[ArchiveInterval.FiveSecond].Sets.Count; i++)
+            foreach (DataSet fivesecond in fiveSecSets)
             {
-                DataSetCollectionWrapper wrapper = new DataSetCollectionWrapper();
-                DataSet fivesecond = ArchiveMaintainer.Archives[ArchiveInterval.FiveSecond].Sets[i];
+                var wrapper = new DataSetCollectionWrapper();
+
                 if (!DataSourceUUIDsToShow.Contains(fivesecond.Uuid))
                     continue;
 
-                Predicate<DataSet> uuidpredicate = new Predicate<DataSet>(delegate(DataSet item) { return item.Uuid == fivesecond.Uuid; });
-
                 wrapper.Sets.Add(ArchiveInterval.FiveSecond, fivesecond);
+                var intervals = new[] {ArchiveInterval.OneMinute, ArchiveInterval.OneHour, ArchiveInterval.OneDay};
 
-                if(ArchiveMaintainer.Archives[ArchiveInterval.OneMinute].Sets.Contains(fivesecond))
-                    wrapper.Sets.Add(ArchiveInterval.OneMinute, ArchiveMaintainer.Archives[ArchiveInterval.OneMinute].Sets.Find(uuidpredicate));
+                foreach (var interval in intervals)
+                {
+                    var found = archives[interval].Sets.FirstOrDefault(s => s.Uuid == fivesecond.Uuid);
+                    if (found != null)
+                        wrapper.Sets.Add(interval, found);
+                }
 
-                if (ArchiveMaintainer.Archives[ArchiveInterval.OneHour].Sets.Contains(fivesecond))
-                    wrapper.Sets.Add(ArchiveInterval.OneHour, ArchiveMaintainer.Archives[ArchiveInterval.OneHour].Sets.Find(uuidpredicate));
-
-                if (ArchiveMaintainer.Archives[ArchiveInterval.OneDay].Sets.Contains(fivesecond))
-                    wrapper.Sets.Add(ArchiveInterval.OneDay, ArchiveMaintainer.Archives[ArchiveInterval.OneDay].Sets.Find(uuidpredicate));
                 wrappers.Add(wrapper);
             }
 
@@ -149,15 +150,15 @@ namespace XenAdmin.Controls.CustomDataGraph
                 }
             }
 
-            CurrentKeys.RemoveAll(new Predicate<DataSetCollectionWrapper>(delegate(DataSetCollectionWrapper item)
+            CurrentKeys.RemoveAll(item =>
             {
-                if(!wrappers.Contains(item))
+                if (!wrappers.Contains(item))
                 {
                     anynew = true;
                     return true;
                 }
                 return false;
-            }));
+            });
 
             if (!anynew)
             {