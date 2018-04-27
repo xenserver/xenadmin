7ba8546929eb94dd0882d4bd0a588a9ae542b8f5
Mihaela Stoica <mihaela.stoica@citrix.com>
2018-04-27 12:13:10 +0100

    CA-286291: Refactoring in VMOperationToolStripManuItem
    - added an action for the API call that retrieves the WLB VM recommendations
    - removed the thread used for updating the menu items, so that we always access the menu items on the UI thread and do only the server calls on separate threads (for WLB, getting the recommendations now is done on the new action, for non-WLB we have the `workerQueueWithoutWlb`)
    
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

---
+++ XenModel/Actions/WLB/WlbRetrieveVmRecommendationsAction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using XenAdmin.Core;
+using XenAdmin.Network;
+using XenAPI;
+
+namespace XenAdmin.Actions
+{
+    public class WlbRetrieveVmRecommendationsAction: PureAsyncAction
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly List<VM> vms;
+        private readonly Dictionary<VM, Dictionary<XenRef<Host>, string[]>> recommendations = new Dictionary<VM, Dictionary<XenRef<Host>, string[]>>();
+        private bool isError;
+
+        public WlbRetrieveVmRecommendationsAction(IXenConnection connection, List<VM> vms)
+            : base(connection, Messages.WLB_RETRIEVING_VM_RECOMMENDATIONS, true)
+        {
+            this.vms = vms;
+        }
+
+        protected override void Run()
+        {
+            if (vms.Count == 0)
+            {
+                isError = true;
+                return;
+            }
+
+            if (Helpers.WlbEnabled(vms[0].Connection))
+            {
+                try
+                {
+                    foreach (var vm in vms)
+                    {
+                        recommendations[vm] = VM.retrieve_wlb_recommendations(Session, vm.opaque_ref);
+                    }
+                }
+                catch (Exception e)
+                {
+                    log.Error("Error getting WLB recommendations", e);
+                    isError = true;
+                }
+            }
+            else
+            {
+                isError = true;
+            }
+        }
+
+        public Dictionary<VM, Dictionary<XenRef<Host>, string[]>> Recommendations
+        {
+            get { return isError ? null : recommendations; }
+        }
+    }
+}
