3e0691fef3b81038c835ed26bc308c59ff2057ce
Michael Z <michael2012zhao@hotmail.com>
2018-07-19 14:11:48 +0800

    Fix wrong health check service name.
    Signed-off-by: Michael Z <michael2012zhao@hotmail.com>

--- XenServerHealthCheck/ProjectInstaller.Designer.cs
+++ XenServerHealthCheck/ProjectInstaller.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.XenServerHealthCheckInstaller.Description = Branding.COMPANY_AND_PRODUCT_BRAND + " Health Check";
             this.XenServerHealthCheckInstaller.DisplayName = Branding.COMPANY_AND_PRODUCT_BRAND + " Health Check";
-            this.XenServerHealthCheckInstaller.ServiceName = string.Join("", Branding.PRODUCT_BRAND.Split(' ')) + "HealthCheck";
+            this.XenServerHealthCheckInstaller.ServiceName = "XenServerHealthCheck";
             this.XenServerHealthCheckInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             this.XenServerHealthCheckInstaller.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.XenServerHealthCheckInstaller_AfterInstall);
             // 
