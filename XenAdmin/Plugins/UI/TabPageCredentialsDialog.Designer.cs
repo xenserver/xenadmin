3de03f32d3cfbaa9df5f0a0d3f3c2e201c7669e5
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2018-08-02 17:13:28 +0100

    Simplified retrieval of credentials from authentication prompt. Placed TabPageCredentialsDialog controls on a TableLayout panel.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Plugins/UI/TabPageCredentialsDialog.Designer.cs
+++ XenAdmin/Plugins/UI/TabPageCredentialsDialog.Designer.cs
@@ -37,11 +37,14 @@
             this.OKButton = new System.Windows.Forms.Button();
             this.CancelBtn = new System.Windows.Forms.Button();
             this.PersistCredentialsCheckBox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TopLabel
             // 
             resources.ApplyResources(this.TopLabel, "TopLabel");
+            this.tableLayoutPanel1.SetColumnSpan(this.TopLabel, 3);
             this.TopLabel.Name = "TopLabel";
             // 
             // UsernameLabel
@@ -57,11 +60,13 @@
             // UsernameTextBox
             // 
             resources.ApplyResources(this.UsernameTextBox, "UsernameTextBox");
+            this.tableLayoutPanel1.SetColumnSpan(this.UsernameTextBox, 2);
             this.UsernameTextBox.Name = "UsernameTextBox";
             // 
             // PasswordTextBox
             // 
             resources.ApplyResources(this.PasswordTextBox, "PasswordTextBox");
+            this.tableLayoutPanel1.SetColumnSpan(this.PasswordTextBox, 2);
             this.PasswordTextBox.Name = "PasswordTextBox";
             this.PasswordTextBox.UseSystemPasswordChar = true;
             // 
@@ -84,8 +89,22 @@
             resources.ApplyResources(this.PersistCredentialsCheckBox, "PersistCredentialsCheckBox");
             this.PersistCredentialsCheckBox.Checked = true;
             this.PersistCredentialsCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tableLayoutPanel1.SetColumnSpan(this.PersistCredentialsCheckBox, 3);
             this.PersistCredentialsCheckBox.Name = "PersistCredentialsCheckBox";
             this.PersistCredentialsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.TopLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CancelBtn, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.UsernameLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OKButton, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.PasswordTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.PasswordLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.UsernameTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.PersistCredentialsCheckBox, 0, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             // 
             // TabPageCredentialsDialog
             // 
@@ -93,17 +112,11 @@
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.CancelButton = this.CancelBtn;
-            this.Controls.Add(this.PersistCredentialsCheckBox);
-            this.Controls.Add(this.CancelBtn);
-            this.Controls.Add(this.OKButton);
-            this.Controls.Add(this.PasswordTextBox);
-            this.Controls.Add(this.UsernameTextBox);
-            this.Controls.Add(this.PasswordLabel);
-            this.Controls.Add(this.UsernameLabel);
-            this.Controls.Add(this.TopLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "TabPageCredentialsDialog";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -117,5 +130,6 @@
         private System.Windows.Forms.Button OKButton;
         private System.Windows.Forms.Button CancelBtn;
         private System.Windows.Forms.CheckBox PersistCredentialsCheckBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
