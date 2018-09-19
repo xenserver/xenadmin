48fc0e5a3f5c7c18225688ea1d6fec05baaaa7a5
Aaron Robson <aaron.robson@citrix.com>
2018-09-19 12:49:47 +0100

    Included a DataGridViewEx component to eventually replace the ListView.
    Signed-off-by: Aaron Robson <aaron.robson@citrix.com>

--- XenAdmin/Dialogs/NewTagDialog.Designer.cs
+++ XenAdmin/Dialogs/NewTagDialog.Designer.cs
@@ -30,16 +30,21 @@ namespace XenAdmin.Dialogs
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewTagDialog));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.NewTagLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.addButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
             this.tagsListView = new System.Windows.Forms.ListView();
-            this.columnHeader = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.stateImageList = new System.Windows.Forms.ImageList(this.components);
             this.renameButton = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
+            this.tagsDataGrid = new XenAdmin.Controls.DataGridViewEx.DataGridViewEx();
+            this.ColumnEnabled = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColumnTags = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tagsDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // NewTagLabel
@@ -50,7 +55,6 @@ namespace XenAdmin.Dialogs
             // textBox1
             // 
             resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.MinimumSize = new System.Drawing.Size(168, 23);
             this.textBox1.Name = "textBox1";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -85,9 +89,9 @@ namespace XenAdmin.Dialogs
             this.tagsListView.StateImageList = this.stateImageList;
             this.tagsListView.UseCompatibleStateImageBehavior = false;
             this.tagsListView.View = System.Windows.Forms.View.Details;
-            this.tagsListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tagsListView_MouseClick);
             this.tagsListView.SelectedIndexChanged += new System.EventHandler(this.tagsListView_SelectedIndexChanged);
             this.tagsListView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tagsListView_KeyDown);
+            this.tagsListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tagsListView_MouseClick);
             // 
             // columnHeader
             // 
@@ -113,11 +117,44 @@ namespace XenAdmin.Dialogs
             this.deleteButton.UseVisualStyleBackColor = true;
             this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
             // 
+            // tagsDataGrid
+            // 
+            this.tagsDataGrid.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.tagsDataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.tagsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tagsDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnEnabled,
+            this.ColumnTags});
+            resources.ApplyResources(this.tagsDataGrid, "tagsDataGrid");
+            this.tagsDataGrid.MultiSelect = true;
+            this.tagsDataGrid.Name = "tagsDataGrid";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tagsDataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            // 
+            // ColumnEnabled
+            // 
+            this.ColumnEnabled.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            resources.ApplyResources(this.ColumnEnabled, "ColumnEnabled");
+            this.ColumnEnabled.Name = "ColumnEnabled";
+            // 
+            // ColumnTags
+            // 
+            this.ColumnTags.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            resources.ApplyResources(this.ColumnTags, "ColumnTags");
+            this.ColumnTags.Name = "ColumnTags";
+            // 
             // NewTagDialog
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.CancelButton = this.cancelButton;
+            this.Controls.Add(this.tagsDataGrid);
             this.Controls.Add(this.tagsListView);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.renameButton);
@@ -130,6 +167,7 @@ namespace XenAdmin.Dialogs
             this.Name = "NewTagDialog";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Activated += new System.EventHandler(this.NewTagDialog_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.tagsDataGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,5 +185,8 @@ namespace XenAdmin.Dialogs
         private System.Windows.Forms.Button renameButton;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.ImageList stateImageList;
+        private Controls.DataGridViewEx.DataGridViewEx tagsDataGrid;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnEnabled;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTags;
     }
 }
