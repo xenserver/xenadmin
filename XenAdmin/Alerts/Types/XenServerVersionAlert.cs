525034d23382727fb40caf74ec730dea393ffa84
Aaron Robson <aaron.robson@citrix.com>
2018-10-22 14:34:31 +0100

    CP-14807: Delete OEM in ServerVersion (#2270)
    * CP-14807: Delete OEM in ServerVersion
    
    * CP-14807: Removing unused IgnorePatchAction and IgnoreServerAction classes (moving their constants to the Updates class).
    
    Signed-off-by: Aaron Robson <aaron.robson@citrix.com>

--- XenAdmin/Alerts/Types/XenServerVersionAlert.cs
+++ XenAdmin/Alerts/Types/XenServerVersionAlert.cs
@@ -105,10 +105,10 @@ namespace XenAdmin.Alerts
 
             Dictionary<string, string> other_config = pool.other_config;
 
-            if (other_config.ContainsKey(IgnoreServerAction.LAST_SEEN_SERVER_VERSION_KEY))
+            if (other_config.ContainsKey(Updates.LAST_SEEN_SERVER_VERSION_KEY))
             {
-                List<string> current = new List<string>(other_config[IgnoreServerAction.LAST_SEEN_SERVER_VERSION_KEY].Split(','));
-                if (current.Contains(Version.VersionAndOEM))
+                List<string> current = new List<string>(other_config[Updates.LAST_SEEN_SERVER_VERSION_KEY].Split(','));
+                if (current.Contains(Version.Version.ToString()))
                     return true;
             }
             return false;
@@ -118,7 +118,7 @@ namespace XenAdmin.Alerts
         {
             if (other is XenServerVersionAlert)
             {
-                return Version.VersionAndOEM == ((XenServerVersionAlert)other).Version.VersionAndOEM;
+                return Version.Version.ToString() == ((XenServerVersionAlert)other).Version.Version.ToString();
             }
             return base.Equals(other);
         }
