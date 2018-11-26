4ceb97799c19e46a91c760e8b4d6b3f3553df981
Aaron Robson <aaron.robson@citrix.com>
2018-11-26 12:27:14 +0000

    CA-303517: Refactored redundant IP Address parsing code from XenAPI-Extensions/VIF.IPAddresses and XenSeach/Common.IPAddressProperty into the purely functional XenAPI-Extensions/Address with unit tests.
    Signed-off-by: Aaron Robson <aaron.robson@citrix.com>

---
+++ XenAdminTests/XenModelTests/AddressTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using XenAPI;
+
+namespace XenAdminTests.XenModelTests
+{
+    [TestFixture, Category(TestCategories.Unit)]
+
+    public class AddressTests
+    {
+        private readonly Dictionary<string, string> _simpleNetworks = new Dictionary<string, string>
+        {
+            {"0/ip", "10.71.57.53"},
+            {"0/ipv4/0", "10.71.57.53"},
+            {"0/ipv6/0", "fe80:0000:0000:0000:1c1a:c2ff:fe2e:c823"}
+        };
+
+        [Test]
+        public void TestSimple()
+        {
+            var expected = new List<string>
+            {
+                "10.71.57.53",
+                "fe80:0000:0000:0000:1c1a:c2ff:fe2e:c823"
+            };
+            var actual = Address.FindIpAddresses(_simpleNetworks, "0");
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void TestUnusedDevice()
+        {
+            var actual = Address.FindIpAddresses(_simpleNetworks, "1");
+            Assert.That(actual, Is.Empty);
+        }
+    }
+}
---
+++ XenModel/XenAPI-Extensions/Address.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XenAPI
+{
+    public abstract class Address
+    {
+        public static List<string> FindIpAddresses(Dictionary<string, string> networks, string device)
+        {
+            if (networks == null || string.IsNullOrWhiteSpace(device))
+                return new List<string>();
+
+            // PR-1373 - VM_guest_metrics.networks is a dictionary of IP addresses in the format:
+            // [["0/ip", <IPv4 address>], 
+            //  ["0/ipv4/0", <IPv4 address>], ["0/ipv4/1", <IPv4 address>],
+            //  ["0/ipv6/0", <IPv6 address>], ["0/ipv6/1", <IPv6 address>]]
+
+            return
+                (from network in networks
+                    where network.Key.StartsWith(string.Format("{0}/ip", device))
+                    orderby network.Key
+                    select network.Value).Distinct().ToList();
+        }
+    }
+}
