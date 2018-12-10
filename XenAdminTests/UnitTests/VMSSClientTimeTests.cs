fdbf76d1b0c344b0fbeda05b97215bdff01be2e1
Aaron Robson <aaron.robson@citrix.com>
2018-12-10 17:33:43 +0200

    CA-301907: FindScheduleWithGivenTimeOffset (with unit tests) currently only implemented with hourly frequency.
    Signed-off-by: Aaron Robson <aaron.robson@citrix.com>

---
+++ XenAdminTests/UnitTests/VMSSClientTimeTests.cs
@@ -0,0 +1,104 @@
+﻿/* Copyright (c) Citrix Systems, Inc. 
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, 
+ * with or without modification, are permitted provided 
+ * that the following conditions are met: 
+ * 
+ * *   Redistributions of source code must retain the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer. 
+ * *   Redistributions in binary form must reproduce the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer in the documentation and/or other 
+ *     materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using XenAPI;
+
+namespace XenAdminTests.UnitTests
+{
+    [TestFixture, Category(TestCategories.Unit)]
+    public class VMSSClientTimeTests
+    {
+        private readonly Dictionary<string, string> _givenSettings = new Dictionary<string, string>
+        {
+            { "hour", "0" },
+            { "min", "15" },
+            { "days", "Monday" }
+        };
+
+        [Test]
+        public void Hourly_with_zero_diff()
+        {
+            var actual = VMSSClient.FindScheduleWithGivenTimeOffset(TimeSpan.Zero, vmss_frequency.hourly, _givenSettings);
+            var expected = _givenSettings;
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Hourly_with_quarter_of_an_hour_diff()
+        {
+            var actual = VMSSClient.FindScheduleWithGivenTimeOffset(TimeSpan.FromMinutes(15), vmss_frequency.hourly, _givenSettings);
+            var expected = new Dictionary<string, string>
+            {
+                { "hour", "0" },
+                { "min", "30" },
+                { "days", "Monday" }
+            };
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Hourly_with_minus_half_an_hour_diff()
+        {
+            var actual = VMSSClient.FindScheduleWithGivenTimeOffset(TimeSpan.FromMinutes(-30), vmss_frequency.hourly, _givenSettings);
+            var expected = new Dictionary<string, string>
+            {
+                { "hour", "0" },
+                { "min", "45" },
+                { "days", "Monday" }
+            };
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Hourly_with_three_quarters_of_an_hour_diff()
+        {
+            var actual = VMSSClient.FindScheduleWithGivenTimeOffset(TimeSpan.FromMinutes(45), vmss_frequency.hourly, _givenSettings);
+            var expected = new Dictionary<string, string>
+            {
+                { "hour", "0" },
+                { "min", "0" },
+                { "days", "Monday" }
+            };
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Hourly_with_an_hour_diff()
+        {
+            var actual = VMSSClient.FindScheduleWithGivenTimeOffset(TimeSpan.FromMinutes(60), vmss_frequency.hourly, _givenSettings);
+            var expected = _givenSettings;
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ XenModel/XenAPI-Extensions/VMSSClientTime.cs
@@ -0,0 +1,75 @@
+﻿/* Copyright (c) Citrix Systems, Inc. 
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, 
+ * with or without modification, are permitted provided 
+ * that the following conditions are met: 
+ * 
+ * *   Redistributions of source code must retain the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer. 
+ * *   Redistributions in binary form must reproduce the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer in the documentation and/or other 
+ *     materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace XenAPI
+{
+    public static class VMSSClient
+    {
+        public static Dictionary<string, string> FindScheduleWithGivenTimeOffset(
+            TimeSpan timeDiff,
+            vmss_frequency frequency,
+            Dictionary<string, string> schedule)
+        {
+            switch (frequency)
+            {
+                case vmss_frequency.hourly:
+                    var output = new Dictionary<string, string>();
+
+                    if (schedule.TryGetValue("hour", out var hour))
+                        output["hour"] = hour;
+
+                    if (schedule.TryGetValue("min", out var min))
+                    {
+                        var newMin = (int.Parse(min) + Convert.ToInt32(timeDiff.TotalMinutes)) % 60;
+                        if (newMin < 0)
+                            newMin = 60 - Math.Abs(newMin);
+                        output["min"] = newMin.ToString();
+                    }
+
+                    if (schedule.TryGetValue("days", out var days))
+                        output["days"] = days;
+
+                    return output;
+                case vmss_frequency.daily:
+                    throw new NotImplementedException("Unhandled vmss_frequency value.");
+                case vmss_frequency.weekly:
+                    throw new NotImplementedException("Unhandled vmss_frequency value.");
+                case vmss_frequency.unknown:
+                    return schedule;
+                default:
+                    throw new ArgumentException("Unhandled vmss_frequency value.");
+            }
+        }
+    }
+}
