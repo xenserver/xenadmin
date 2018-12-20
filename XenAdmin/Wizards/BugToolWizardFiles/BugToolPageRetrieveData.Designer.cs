7d99f0b4a393e00061299cde69e0621a1b6ff409
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2018-12-20 12:19:44 +0000

    CA-303195: Rewrote the logic for compiling and retrieving server status data. The changes include: - removal of the SystemStatusAction launching the individual SingleHostStatusActions, because the connection was set incorrectly to the last one of the selected connections and, if an error occurred to that connection, the data collection for all of them failed. - proper handling of HTTP.CancelledExceptions. - replacement of the FlickerFreeListBox by the more versatile DataGridViewEx. - corrections to reporting progress for individual hosts as well as page totals. - made collection of client side data more obvious.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Wizards/BugToolWizardFiles/BugToolPageRetrieveData.Designer.cs
+++ XenAdmin/Wizards/BugToolWizardFiles/BugToolPageRetrieveData.Designer.cs
@@ -13,9 +13,19 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing)
             {
-                components.Dispose();
+                foreach (var r in dataGridViewEx1.Rows)
+                {
+                    if (r is StatusReportRow row)
+                    {
+                        row.DeRegisterEvents();
+                        DeRegisterRowEvents(row);
+                    }
+                }
+
+                if (components != null)
+                    components.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -29,45 +39,99 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BugToolPageRetrieveData));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelBlurb = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.flickerFreeListBox1 = new XenAdmin.Controls.FlickerFreeListBox();
+            this.dataGridViewEx1 = new XenAdmin.Controls.DataGridViewEx.DataGridViewEx();
+            this.columnHostImage = new System.Windows.Forms.DataGridViewImageColumn();
+            this.columnHost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnHostStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnResultImage = new System.Windows.Forms.DataGridViewImageColumn();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEx1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelBlurb, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelError, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.flickerFreeListBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridViewEx1, 0, 1);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             // 
-            // label2
+            // labelBlurb
             // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
+            resources.ApplyResources(this.labelBlurb, "labelBlurb");
+            this.labelBlurb.Name = "labelBlurb";
             // 
-            // label1
+            // labelError
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.labelError, "labelError");
+            this.labelError.Name = "labelError";
             // 
             // progressBar1
             // 
             resources.ApplyResources(this.progressBar1, "progressBar1");
             this.progressBar1.Name = "progressBar1";
             // 
-            // flickerFreeListBox1
+            // dataGridViewEx1
             // 
-            resources.ApplyResources(this.flickerFreeListBox1, "flickerFreeListBox1");
-            this.flickerFreeListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.flickerFreeListBox1.FormattingEnabled = true;
-            this.flickerFreeListBox1.Name = "flickerFreeListBox1";
-            this.flickerFreeListBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.flickerFreeListBox1_DrawItem);
+            this.dataGridViewEx1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewEx1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridViewEx1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridViewEx1.ColumnHeadersVisible = false;
+            this.dataGridViewEx1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnHostImage,
+            this.columnHost,
+            this.columnHostStatus,
+            this.columnResultImage});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewEx1.DefaultCellStyle = dataGridViewCellStyle3;
+            resources.ApplyResources(this.dataGridViewEx1, "dataGridViewEx1");
+            this.dataGridViewEx1.HideSelection = true;
+            this.dataGridViewEx1.Name = "dataGridViewEx1";
+            // 
+            // columnHostImage
+            // 
+            this.columnHostImage.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle1.NullValue")));
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.columnHostImage.DefaultCellStyle = dataGridViewCellStyle1;
+            resources.ApplyResources(this.columnHostImage, "columnHostImage");
+            this.columnHostImage.Name = "columnHostImage";
+            // 
+            // columnHost
+            // 
+            resources.ApplyResources(this.columnHost, "columnHost");
+            this.columnHost.Name = "columnHost";
+            // 
+            // columnHostStatus
+            // 
+            this.columnHostStatus.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            resources.ApplyResources(this.columnHostStatus, "columnHostStatus");
+            this.columnHostStatus.Name = "columnHostStatus";
+            // 
+            // columnResultImage
+            // 
+            this.columnResultImage.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle2.NullValue")));
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.columnResultImage.DefaultCellStyle = dataGridViewCellStyle2;
+            resources.ApplyResources(this.columnResultImage, "columnResultImage");
+            this.columnResultImage.Name = "columnResultImage";
             // 
             // BugToolPageRetrieveData
             // 
@@ -77,6 +141,7 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             resources.ApplyResources(this, "$this");
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEx1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,9 +149,13 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelError;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private XenAdmin.Controls.FlickerFreeListBox flickerFreeListBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelBlurb;
+        private Controls.DataGridViewEx.DataGridViewEx dataGridViewEx1;
+        private System.Windows.Forms.DataGridViewImageColumn columnHostImage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnHost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnHostStatus;
+        private System.Windows.Forms.DataGridViewImageColumn columnResultImage;
     }
 }
