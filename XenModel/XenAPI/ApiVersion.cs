fc0beeeb2083e411da0fc9ab69dc2f885c74c04f
Mihaela Stoica <mihaela.stoica@citrix.com>
2019-03-08 09:57:16 +0000

    Update API bingings (including the API version bump)
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- XenModel/XenAPI/ApiVersion.cs
+++ XenModel/XenAPI/ApiVersion.cs
@@ -59,8 +59,9 @@ namespace XenAPI
         API_2_9 = 20, //XenServer 7.4 (jura)
         API_2_10 = 21, //XenServer 7.5 (kolkata)
         API_2_11 = 22, //XenServer 7.6 (lima)
-        API_2_12 = 23, //Unreleased (naples)
-        LATEST = 23,
+        API_2_12 = 23, //Citrix Hypervisor 8.0 (naples)
+        API_2_13 = 24, //Unreleased (oslo)
+        LATEST = 24,
         UNKNOWN = 99
     }
 
@@ -116,6 +117,8 @@ namespace XenAPI
                     return "2.11";
                 case API_Version.API_2_12:
                     return "2.12";
+                case API_Version.API_2_13:
+                    return "2.13";
                 default:
                     return "Unknown";
             }
@@ -170,4 +173,4 @@ namespace XenAPI
             return APIVersionCompare(session, v) >= 0;
         }
     }
-}
+}
