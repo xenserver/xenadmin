12cb7b780330cacc8621b195aa42437dc399e273
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2019-03-22 14:06:27 +0000

    CP-30918: Show warnings if GFS2 SRs are not used with multipath.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Wizards/NewSRWizard_Pages/ChooseSrProvisioningPage.Designer.cs
+++ XenAdmin/Wizards/NewSRWizard_Pages/ChooseSrProvisioningPage.Designer.cs
@@ -36,12 +36,17 @@
             this.radioButtonLvm = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
             this.tableLayoutInfo = new System.Windows.Forms.TableLayoutPanel();
-            this.labelWarning = new System.Windows.Forms.Label();
             this.pictureBoxInfo = new System.Windows.Forms.PictureBox();
+            this.labelInfo = new System.Windows.Forms.Label();
             this.linkLabelPoolProperties = new System.Windows.Forms.LinkLabel();
+            this.tableLayoutWarning = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxWarning = new System.Windows.Forms.PictureBox();
+            this.labelWarning = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutInfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).BeginInit();
+            this.tableLayoutWarning.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWarning)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -53,6 +58,7 @@
             this.tableLayoutPanel1.Controls.Add(this.radioButtonLvm, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutInfo, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutWarning, 0, 6);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             // 
             // label1
@@ -67,6 +73,7 @@
             this.radioButtonGfs2.Name = "radioButtonGfs2";
             this.radioButtonGfs2.TabStop = true;
             this.radioButtonGfs2.UseVisualStyleBackColor = true;
+            this.radioButtonGfs2.CheckedChanged += new System.EventHandler(this.radioButtonGfs2_CheckedChanged);
             // 
             // labelGFS2
             // 
@@ -78,6 +85,7 @@
             resources.ApplyResources(this.radioButtonLvm, "radioButtonLvm");
             this.radioButtonLvm.Name = "radioButtonLvm";
             this.radioButtonLvm.UseVisualStyleBackColor = true;
+            this.radioButtonLvm.CheckedChanged += new System.EventHandler(this.radioButtonLvm_CheckedChanged);
             // 
             // label3
             // 
@@ -87,15 +95,10 @@
             // tableLayoutInfo
             // 
             resources.ApplyResources(this.tableLayoutInfo, "tableLayoutInfo");
-            this.tableLayoutInfo.Controls.Add(this.labelWarning, 1, 0);
             this.tableLayoutInfo.Controls.Add(this.pictureBoxInfo, 0, 0);
+            this.tableLayoutInfo.Controls.Add(this.labelInfo, 1, 0);
             this.tableLayoutInfo.Controls.Add(this.linkLabelPoolProperties, 2, 0);
             this.tableLayoutInfo.Name = "tableLayoutInfo";
-            // 
-            // labelWarning
-            // 
-            resources.ApplyResources(this.labelWarning, "labelWarning");
-            this.labelWarning.Name = "labelWarning";
             // 
             // pictureBoxInfo
             // 
@@ -104,12 +107,36 @@
             this.pictureBoxInfo.Name = "pictureBoxInfo";
             this.pictureBoxInfo.TabStop = false;
             // 
+            // labelInfo
+            // 
+            resources.ApplyResources(this.labelInfo, "labelInfo");
+            this.labelInfo.Name = "labelInfo";
+            // 
             // linkLabelPoolProperties
             // 
             resources.ApplyResources(this.linkLabelPoolProperties, "linkLabelPoolProperties");
             this.linkLabelPoolProperties.Name = "linkLabelPoolProperties";
             this.linkLabelPoolProperties.TabStop = true;
             this.linkLabelPoolProperties.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelPoolProperties_LinkClicked);
+            // 
+            // tableLayoutWarning
+            // 
+            resources.ApplyResources(this.tableLayoutWarning, "tableLayoutWarning");
+            this.tableLayoutWarning.Controls.Add(this.pictureBoxWarning, 0, 0);
+            this.tableLayoutWarning.Controls.Add(this.labelWarning, 1, 0);
+            this.tableLayoutWarning.Name = "tableLayoutWarning";
+            // 
+            // pictureBoxWarning
+            // 
+            this.pictureBoxWarning.Image = global::XenAdmin.Properties.Resources._000_WarningAlert_h32bit_32;
+            resources.ApplyResources(this.pictureBoxWarning, "pictureBoxWarning");
+            this.pictureBoxWarning.Name = "pictureBoxWarning";
+            this.pictureBoxWarning.TabStop = false;
+            // 
+            // labelWarning
+            // 
+            resources.ApplyResources(this.labelWarning, "labelWarning");
+            this.labelWarning.Name = "labelWarning";
             // 
             // ChooseSrProvisioningPage
             // 
@@ -122,6 +149,9 @@
             this.tableLayoutInfo.ResumeLayout(false);
             this.tableLayoutInfo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).EndInit();
+            this.tableLayoutWarning.ResumeLayout(false);
+            this.tableLayoutWarning.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWarning)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,8 +165,11 @@
         private System.Windows.Forms.RadioButton radioButtonLvm;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBoxInfo;
-        private System.Windows.Forms.Label labelWarning;
+        private System.Windows.Forms.Label labelInfo;
         private System.Windows.Forms.TableLayoutPanel tableLayoutInfo;
         private System.Windows.Forms.LinkLabel linkLabelPoolProperties;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutWarning;
+        private System.Windows.Forms.PictureBox pictureBoxWarning;
+        private System.Windows.Forms.Label labelWarning;
     }
 }
