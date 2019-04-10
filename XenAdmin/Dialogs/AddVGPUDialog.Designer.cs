a09c020dfe62f61efbdc43b64cc3c0f80b84234e
Michael Z <michael.zhao@citrix.com>
2019-04-10 14:35:43 +0800

    AddVGPUDialog
    Signed-off-by: Michael Z <michael.zhao@citrix.com>

---
+++ XenAdmin/Dialogs/AddVGPUDialog.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace XenAdmin.Dialogs
+{
+    partial class AddVGPUDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.comboBoxTypes = new XenAdmin.Controls.VgpuComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.buttonAdd.Location = new System.Drawing.Point(244, 77);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.buttonCancel.Location = new System.Drawing.Point(325, 77);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // comboBoxTypes
+            // 
+            this.comboBoxTypes.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBoxTypes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTypes.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.comboBoxTypes.FormattingEnabled = true;
+            this.comboBoxTypes.Location = new System.Drawing.Point(12, 28);
+            this.comboBoxTypes.Name = "comboBoxTypes";
+            this.comboBoxTypes.Size = new System.Drawing.Size(388, 24);
+            this.comboBoxTypes.TabIndex = 3;
+            // 
+            // AddVGPUDialog
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(412, 118);
+            this.Controls.Add(this.comboBoxTypes);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Name = "AddVGPUDialog";
+            this.Text = "AddVGPUDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCancel;
+        private Controls.VgpuComboBox comboBoxTypes;
+    }
+}
---
+++ XenAdmin/Dialogs/AddVGPUDialog.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XenAdmin.Actions;
+using XenAdmin.Controls;
+using XenAdmin.Core;
+using XenAdmin.Properties;
+using XenAPI;
+
+namespace XenAdmin.Dialogs
+{
+    public partial class AddVGPUDialog : XenDialogBase
+    {
+        private VM _vm;
+        private GPU_group[] gpu_groups;
+
+        public AddVGPUDialog(VM vm)
+        {
+            _vm = vm;
+            InitializeComponent();
+            BuildList();
+        }
+
+        private void BuildList()
+        {
+            gpu_groups = _vm.Connection.Cache.GPU_groups.Where(g => g.PGPUs.Count > 0 && g.supported_VGPU_types.Count != 0).ToArray();
+            PopulateComboBox();
+        }
+
+        public void SetXenObjects(IXenObject orig, IXenObject clone)
+        {
+            Trace.Assert(clone is VM);  // only VMs should show this page
+            Trace.Assert(!Helpers.FeatureForbidden(clone, Host.RestrictGpu));  // If license insufficient, we show upsell page instead
+
+            _vm = (VM)clone;
+
+            PopulateComboBox();
+        }
+
+        private void PopulateComboBox()
+        {
+            var noneItem = new GpuTuple(null, null, null); // "None" item
+            comboBoxTypes.Items.Add(noneItem);
+
+            Array.Sort(gpu_groups);
+            foreach (GPU_group gpu_group in gpu_groups)
+            {
+                if (Helpers.FeatureForbidden(_vm.Connection, Host.RestrictVgpu) || !_vm.CanHaveVGpu())
+                {
+                    if (gpu_group.HasPassthrough())
+                        comboBoxTypes.Items.Add(new GpuTuple(gpu_group, null, null));  //GPU pass-through item
+                }
+                else
+                {
+                    var enabledTypes = _vm.Connection.ResolveAll(gpu_group.enabled_VGPU_types);
+                    var allTypes = _vm.Connection.ResolveAll(gpu_group.supported_VGPU_types);
+
+                    var disabledTypes = allTypes.FindAll(t => !enabledTypes.Exists(e => e.opaque_ref == t.opaque_ref));
+
+                    if (gpu_group.HasVGpu())
+                    {
+                        allTypes.Sort();
+                        allTypes.Reverse();
+                        comboBoxTypes.Items.Add(new GpuTuple(gpu_group, allTypes.ToArray())); // Group item
+                    }
+
+                    foreach (var vgpuType in allTypes)
+                        comboBoxTypes.Items.Add(new GpuTuple(gpu_group, vgpuType, disabledTypes.ToArray())); // GPU_type item
+
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
