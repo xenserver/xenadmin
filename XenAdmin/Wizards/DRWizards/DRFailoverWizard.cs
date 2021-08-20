7109b388bd5fcd0f12f1e99d9300ed5d231bf4c6
Danilo Del Busso <Danilo.Del.Busso@citrix.com>
2021-08-20 20:58:12 +0100

    CP-34231: Update if block to exclude unecessary else in DRFailoverWizard.cs
    Not relevant to the PR, but the change is small enought to no need extra testing
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>

--- XenAdmin/Wizards/DRWizards/DRFailoverWizard.cs
+++ XenAdmin/Wizards/DRWizards/DRFailoverWizard.cs
@@ -83,10 +83,8 @@ namespace XenAdmin.Wizards.DRWizards
             WizardType = wizardType; 
 
             #region RBAC Warning Page Checks
-            if (Pool.Connection.Session.IsLocalSuperuser || Helpers.GetCoordinator(Pool.Connection).external_auth_type == Auth.AUTH_TYPE_NONE)
-            {
-            }
-            else
+            if (!Pool.Connection.Session.IsLocalSuperuser &&
+                Helpers.GetCoordinator(Pool.Connection).external_auth_type != Auth.AUTH_TYPE_NONE)
             {
                 RBACWarningPage.WizardPermissionCheck check = new RBACWarningPage.WizardPermissionCheck(Messages.RBAC_DR_WIZARD_MESSAGE);
                 check.AddApiCheck("DR_task.async_create");
