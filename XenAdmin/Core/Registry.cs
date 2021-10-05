84ac5da4098f8efc18f5a9df11171136147f268a
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2021-10-05 13:32:30 +0100

    Guard against registry keys returning empty strings.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Core/Registry.cs
+++ XenAdmin/Core/Registry.cs
@@ -140,13 +140,16 @@ namespace XenAdmin.Core
         }
 
         /// <summary>
-        /// Reads a string value k under XENCENTER_LOCAL_KEYS, targeting the 32-bit
-        /// registry view, and trying CurrentUser first and then LocalMachine
+        /// Reads a string value k under XENCENTER_LOCAL_KEYS, trying CurrentUser first and then LocalMachine
         /// </summary>
-        private static string ReadInstalledKey(string k)
+        private static string ReadInstalledKey(string k, RegistryView rView = RegistryView.Default)
         {
-            return ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, k, RegistryView.Registry32) ??
-                   ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, k, RegistryView.Registry32);
+            var val = ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, k, rView);
+
+            if (string.IsNullOrEmpty(val))
+                val = ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, k, rView);
+
+            return string.IsNullOrEmpty(val) ? null : val;
         }
 
         public static string HealthCheckIdentityTokenDomainName => ReadString(HEALTH_CHECK_IDENTITY_TOKEN_DOMAIN_NAME);
@@ -161,32 +164,28 @@ namespace XenAdmin.Core
 
         public static string HealthCheckProductKey => ReadString(HEALTH_CHECK_PRODUCT_KEY);
 
-        public static string HiddenFeatures => ReadInstalledKey(HIDDEN_FEATURES);
+        public static string HiddenFeatures => ReadInstalledKey(HIDDEN_FEATURES, RegistryView.Registry32);
 
-        public static string AdditionalFeatures => ReadInstalledKey(ADDITIONAL_FEATURES);
+        public static string AdditionalFeatures => ReadInstalledKey(ADDITIONAL_FEATURES, RegistryView.Registry32);
 
         public static string GetCustomUpdatesXmlLocation()
         {
-            return ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, CUSTOM_UPDATES_XML_LOCATION) ??
-                   ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, CUSTOM_UPDATES_XML_LOCATION);
+            return ReadInstalledKey(CUSTOM_UPDATES_XML_LOCATION);
         }
 
         public static string GetCustomFileServicePrefix()
         {
-            return ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, CUSTOM_FILESERVICE_PREFIX) ??
-                   ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, CUSTOM_FILESERVICE_PREFIX);
+            return ReadInstalledKey(CUSTOM_FILESERVICE_PREFIX);
         }
 
         public static string GetCustomClientIdUrl()
         {
-            return ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, CUSTOM_CLIENT_ID_URL) ??
-                   ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, CUSTOM_CLIENT_ID_URL);
+            return ReadInstalledKey(CUSTOM_CLIENT_ID_URL);
         }
 
         public static string GetCustomTokenUrl()
         {
-            return ReadRegistryValue(RegistryHive.CurrentUser, XENCENTER_LOCAL_KEYS, CUSTOM_TOKEN_URL) ??
-                   ReadRegistryValue(RegistryHive.LocalMachine, XENCENTER_LOCAL_KEYS, CUSTOM_TOKEN_URL);
+            return ReadInstalledKey(CUSTOM_TOKEN_URL);
         }
 
         public static string CustomHelpUrl => ReadString(HELP_URL_OVERRIDE);
