844124d74a19a12fc29e38c03577a977e5c035f2
Danilo Del Busso <Danilo.Del.Busso@citrix.com>
2021-11-25 11:58:56 +0000

    CP-36392: Specify `namespace` for `SettingsUpdate`
    Added to `XenAdmin`
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>

--- XenAdmin/SettingsUpdate.cs
+++ XenAdmin/SettingsUpdate.cs
@@ -30,83 +30,85 @@
  */
 
 using System;
-using System.Reflection;
-using XenAdmin;
 using System.IO;
+using System.Reflection;
 
-public static class SettingsUpdate
+namespace XenAdmin
 {
-    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-    // The path of the user.config files looks something like this:
-    // <Profile Directory>\<Company Name>\<App Name>_<Evidence Type>_<Evidence Hash>\<Version>\user.config
-
-    /// <summary>
-    /// Looks for a config file from a previous installation of the application and updates the settings from it.
-    /// </summary>
-    public static void Update()
+    public static class SettingsUpdate
     {
-        try
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        // The path of the user.config files looks something like this:
+        // <Profile Directory>\<Company Name>\<App Name>_<Evidence Type>_<Evidence Hash>\<Version>\user.config
+
+        /// <summary>
+        /// Looks for a config file from a previous installation of the application and updates the settings from it.
+        /// </summary>
+        public static void Update()
         {
-            Assembly a = Assembly.GetExecutingAssembly();
-            Version appVersion = a.GetName().Version;
-
-            // get previous config file by enumerating through all the folders in <Profile Directory>\<Company Name> 
-            // to find a previous user.config file
-
-            var currentConfigFolder = new DirectoryInfo(Settings.GetUserConfigPath()).Parent;
-
-            if (currentConfigFolder == null)
-                return;
-
-            var appDomainName = AppDomain.CurrentDomain.FriendlyName;
-            var companyFolder = currentConfigFolder.Parent != null ? currentConfigFolder.Parent.Parent : null;
-
-            if (companyFolder == null)
-                return;
-
-            FileInfo previousConfig = null;
-            Version previousVersion = null;
-
-            foreach (var subDir in companyFolder.GetDirectories("*" + appDomainName + "*", SearchOption.AllDirectories))
+            try
             {
-                foreach (var file in subDir.GetFiles("user.config", SearchOption.AllDirectories))
-                {
-                    var configFolderName = Path.GetFileName(Path.GetDirectoryName(file.FullName));
-                    if (configFolderName != null)
-                    {
-                        var configVersion = new Version(configFolderName);
+                Assembly a = Assembly.GetExecutingAssembly();
+                Version appVersion = a.GetName().Version;
 
-                        if ((configVersion <= appVersion) && (previousVersion == null || configVersion > previousVersion))
+                // get previous config file by enumerating through all the folders in <Profile Directory>\<Company Name> 
+                // to find a previous user.config file
+
+                var currentConfigFolder = new DirectoryInfo(Settings.GetUserConfigPath()).Parent;
+
+                if (currentConfigFolder == null)
+                    return;
+
+                var appDomainName = AppDomain.CurrentDomain.FriendlyName;
+                var companyFolder = currentConfigFolder.Parent != null ? currentConfigFolder.Parent.Parent : null;
+
+                if (companyFolder == null)
+                    return;
+
+                FileInfo previousConfig = null;
+                Version previousVersion = null;
+
+                foreach (var subDir in companyFolder.GetDirectories("*" + appDomainName + "*", SearchOption.AllDirectories))
+                {
+                    foreach (var file in subDir.GetFiles("user.config", SearchOption.AllDirectories))
+                    {
+                        var configFolderName = Path.GetFileName(Path.GetDirectoryName(file.FullName));
+                        if (configFolderName != null)
                         {
-                            previousVersion = configVersion;
-                            previousConfig = file;
+                            var configVersion = new Version(configFolderName);
+
+                            if ((configVersion <= appVersion) && (previousVersion == null || configVersion > previousVersion))
+                            {
+                                previousVersion = configVersion;
+                                previousConfig = file;
+                            }
                         }
                     }
                 }
-            }
             
-            if (previousConfig != null)
-            {
-                // copy previous config file to current config location
-                var destinationFile = Path.GetDirectoryName(currentConfigFolder.FullName);
+                if (previousConfig != null)
+                {
+                    // copy previous config file to current config location
+                    var destinationFile = Path.GetDirectoryName(currentConfigFolder.FullName);
 
-                destinationFile = Path.Combine(destinationFile, previousVersion.ToString());
+                    destinationFile = Path.Combine(destinationFile, previousVersion.ToString());
 
-                if (!Directory.Exists(destinationFile))
-                    Directory.CreateDirectory(destinationFile);
+                    if (!Directory.Exists(destinationFile))
+                        Directory.CreateDirectory(destinationFile);
 
-                destinationFile = Path.Combine(destinationFile, previousConfig.Name);
+                    destinationFile = Path.Combine(destinationFile, previousConfig.Name);
 
-                File.Copy(previousConfig.FullName, destinationFile);
+                    File.Copy(previousConfig.FullName, destinationFile);
 
-                // upgrade settings
-                XenAdmin.Properties.Settings.Default.Upgrade();
+                    // upgrade settings
+                    XenAdmin.Properties.Settings.Default.Upgrade();
+                }
             }
-        }
-        catch (Exception ex)
-        {
-            log.Debug("Exception while updating settings.", ex);
+            catch (Exception ex)
+            {
+                log.Debug("Exception while updating settings.", ex);
+            }
         }
     }
 }
