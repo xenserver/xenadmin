596f76ddcb6f6944d6f40bdb556d3fd029e336ed
Danilo Del Busso <Danilo.Del.Busso@citrix.com>
2022-01-05 12:14:21 +0100

    CA-361926: Ensure `Messages.resx` in XenCenter stays alphabetically sorted (#2915)
    * CA-361926: Add script to check sorting of Messages.resx
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Add string sorting to PR/push checks
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Add script to sort strings in Messages.resx
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Allow multi-path input for string sorting scripts
    
    Also improved output to help with errors
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Add localization option to string sorting scripts
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Specify encoding in string sorting scripts
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Update `main.yml` to reflect changes to string checking scripts
    
    Also add other `.resx` files
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Remove case sensitive equality for boolean operation
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Rename `Get-Path` into `Get-ResolvedPath`
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Remove unused variable
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Add exit code check before executing string sorting check for `Branding.resx`
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Improve readability of string sorting scripts
    
    Changed spacing using automatic formatter
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>
    
    * CA-361926: Fix exit message in string sorting scripts
    
    Signed-off-by: Danilo Del Busso <Danilo.Del.Busso@citrix.com>

---
+++ scripts/sort_strings.ps1
@@ -0,0 +1,130 @@
+# Copyright (c) Citrix Systems, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms,
+# with or without modification, are permitted provided
+# that the following conditions are met:
+#
+# *   Redistributions of source code must retain the above
+#     copyright notice, this list of conditions and the
+#     following disclaimer.
+# *   Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the
+#     following disclaimer in the documentation and/or other
+#     materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+#Requires -Version 3.0
+
+Param(
+    [Parameter(Mandatory = $true, HelpMessage = "Comma separated paths of the input files")]
+    [String[]]$PATHS,
+    [Parameter(HelpMessage = "Whether to also checks for .ja and .zh-CN resx files in the same directory as the input file. Files must be present.")]
+    [switch]$CHECK_LOCALIZED,
+    [Parameter(HelpMessage = "Whether to list the names of the strings as they're being manipulated")]
+    [switch]$NOISY
+)
+
+#region Functions
+function Test-Paths($paths) {
+    foreach ($path in $paths) {
+        $path = Get-ResolvedPath $path
+
+        if ((Test-Path $path) -eq $false) {
+            Write-Output "File $path does not exist. Exiting."
+            exit 1;
+        }
+
+        if ([IO.Path]::GetExtension($path) -cne ".resx") {
+            Write-Output "$path is not a .resx file. Exiting."
+            exit 1;
+        }
+        
+        if ($CHECK_LOCALIZED) {
+            $fileName = $path.replace(".resx", "")
+            
+            if ((Test-Path "$fileName.ja.resx") -eq $false) {
+                Write-Output "Could not find Japanese localized file for $path. Exiting."
+                exit 1
+            }
+            if ((Test-Path "$fileName.zh-CN.resx") -eq $false ) {
+                Write-Output "Could not find Chinese localized file for $path. Exiting."
+                exit 1
+            }
+        }
+    }
+}
+
+function Update-Strings($path) {
+    Write-Output "Fetching content of $path"
+    
+    [xml]$xml = Get-Content -Encoding "utf8" -Path $path
+    
+    $strings = $xml.root.data 
+    $sortedStrings = $strings | Sort-Object name
+    $count = $strings.length
+    
+    Write-Output "Found $count strings"
+    
+    foreach ($_ in $strings) {
+        if ($NOISY) {
+            Write-Output "Removing string $($_.name)"
+        }
+        # ignore stdout
+        $xml.root.RemoveChild($_) >  $null
+    }
+    
+    Write-Output "Removed unsorted strings"
+    
+    foreach ($_ in $sortedStrings) {
+        if ($NOISY) {
+            Write-Output "Adding string $($_.name)"
+        }
+        # ignore stdout
+        $xml.root.AppendChild($_) >  $null
+    }
+    
+    Write-Output "Added sorted strings"
+    
+    Write-Output "Updating content of $path`n"
+    $xml.Save($path)    
+}
+
+function Get-ResolvedPath($path) {
+    # Resolve relative path
+    $resolvedPath = Resolve-Path $path
+    return $resolvedPath.Path
+}
+
+#endregion
+
+#region Script
+
+Test-Paths $PATHS
+
+foreach ($path in $PATHS) {
+    $path = Get-ResolvedPath $path
+    Update-Strings $path
+    if ($CHECK_LOCALIZED) {
+        $fileName = $path.replace(".resx", "")
+        Update-Strings  "$fileName.ja.resx"
+        Update-Strings "$fileName.zh-CN.resx"
+    }
+    
+}
+
+#endregion
