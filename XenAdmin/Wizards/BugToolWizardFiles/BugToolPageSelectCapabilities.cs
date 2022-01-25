d6f91526d2321f5dc57bfefa3e7d7f218fd6f7d6
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2022-01-25 13:32:55 +0000

    Restored friendly name and description for CVSM.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- XenAdmin/Wizards/BugToolWizardFiles/BugToolPageSelectCapabilities.cs
+++ XenAdmin/Wizards/BugToolWizardFiles/BugToolPageSelectCapabilities.cs
@@ -541,14 +541,18 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             {
                 _key = value;
 
-                _name = FriendlyNameManager.GetFriendlyName(string.Format("Label-host.system_status-{0}", _key));
+                _name = FriendlyNameManager.GetFriendlyName($"Label-host.system_status-{_key}");
+                _description = FriendlyNameManager.GetFriendlyName($"Description-host.system_status-{_key}");
 
                 if (string.IsNullOrEmpty(_name))
                     _name = _key;
                 else if (_key == "client-logs")
                     _name = string.Format(_name, BrandManager.BrandConsole);
-
-                _description = FriendlyNameManager.GetFriendlyName(string.Format("Description-host.system_status-{0}", _key));
+                else if (_key == "CVSM")
+                {
+                    _name = string.Format(_name, BrandManager.CompanyNameShort);
+                    _description = string.Format(_description, BrandManager.CompanyNameShort);
+                }
             }
         }
 
