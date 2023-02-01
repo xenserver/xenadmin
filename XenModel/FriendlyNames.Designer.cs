6fecc41045a5b3c546d9e74538bbcb339bce6f3e
Danilo Del Busso <danilo.delbusso@cloud.com>
2023-02-01 10:24:23 +0000

    CP-40157: Update friendly name for `xapi_open_fds` data source
    Also apply string sorting
    
    Signed-off-by: Danilo Del Busso <danilo.delbusso@cloud.com>

--- XenModel/FriendlyNames.Designer.cs
+++ XenModel/FriendlyNames.Designer.cs
@@ -19,7 +19,7 @@ namespace XenAdmin {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class FriendlyNames {
@@ -2455,7 +2455,7 @@ namespace XenAdmin {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Number of open file descriptors.
+        ///   Looks up a localized string similar to Agent open file descriptors.
         /// </summary>
         public static string Label_performance_xapi_open_fds {
             get {
