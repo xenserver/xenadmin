a8f03975bb8221779fad5d95752b5a60727c3d03
Konstantina Chremmou <Konstantina.Chremmou@cloud.com>
2023-02-01 13:23:24 +0000

    Minor layout tweaks.
    Signed-off-by: Konstantina Chremmou <Konstantina.Chremmou@cloud.com>

--- XenAdmin/Wizards/BugToolWizardFiles/BugToolPageDestination.Designer.cs
+++ XenAdmin/Wizards/BugToolWizardFiles/BugToolPageDestination.Designer.cs
@@ -35,8 +35,8 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             this.m_textBoxName = new System.Windows.Forms.TextBox();
             this.labelFileLocation = new System.Windows.Forms.Label();
             this.m_textBoxLocation = new System.Windows.Forms.TextBox();
-            this.BrowseButton = new System.Windows.Forms.Button();
             this.m_ctrlError = new XenAdmin.Controls.Common.PasswordFailure();
+            this.BrowseButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,14 +48,14 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             this.tableLayoutPanel1.Controls.Add(this.m_textBoxName, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelFileLocation, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.m_textBoxLocation, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.BrowseButton, 3, 2);
-            this.tableLayoutPanel1.Controls.Add(this.m_ctrlError, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.m_ctrlError, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.BrowseButton, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
-            this.tableLayoutPanel1.SetColumnSpan(this.label2, 4);
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 3);
             this.label2.Name = "label2";
             // 
             // labelName
@@ -65,7 +65,6 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             // 
             // m_textBoxName
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.m_textBoxName, 2);
             resources.ApplyResources(this.m_textBoxName, "m_textBoxName");
             this.m_textBoxName.Name = "m_textBoxName";
             this.m_textBoxName.TextChanged += new System.EventHandler(this.m_textBoxName_TextChanged);
@@ -77,10 +76,15 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             // 
             // m_textBoxLocation
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.m_textBoxLocation, 2);
             resources.ApplyResources(this.m_textBoxLocation, "m_textBoxLocation");
             this.m_textBoxLocation.Name = "m_textBoxLocation";
             this.m_textBoxLocation.TextChanged += new System.EventHandler(this.m_textBoxLocation_TextChanged);
+            // 
+            // m_ctrlError
+            // 
+            resources.ApplyResources(this.m_ctrlError, "m_ctrlError");
+            this.tableLayoutPanel1.SetColumnSpan(this.m_ctrlError, 3);
+            this.m_ctrlError.Name = "m_ctrlError";
             // 
             // BrowseButton
             // 
@@ -88,12 +92,6 @@ namespace XenAdmin.Wizards.BugToolWizardFiles
             this.BrowseButton.Name = "BrowseButton";
             this.BrowseButton.UseVisualStyleBackColor = true;
             this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
-            // 
-            // m_ctrlError
-            // 
-            resources.ApplyResources(this.m_ctrlError, "m_ctrlError");
-            this.tableLayoutPanel1.SetColumnSpan(this.m_ctrlError, 4);
-            this.m_ctrlError.Name = "m_ctrlError";
             // 
             // BugToolPageDestination
             // 
