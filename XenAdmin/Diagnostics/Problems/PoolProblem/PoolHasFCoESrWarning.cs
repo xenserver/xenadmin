43e2cecf32dc4fae3252656c2a3365d3c9e53921
Danilo Del Busso <danilo.delbusso@cloud.com>
2023-02-14 10:23:12 +0000

    CP-41737: Update copyright notices
    Signed-off-by: Danilo Del Busso <danilo.delbusso@cloud.com>

--- XenAdmin/Diagnostics/Problems/PoolProblem/PoolHasFCoESrWarning.cs
+++ XenAdmin/Diagnostics/Problems/PoolProblem/PoolHasFCoESrWarning.cs
@@ -1,6 +1,5 @@
-﻿/* Copyright (c) Citrix Systems, Inc. 
- * All rights reserved. 
- * 
+﻿/* Copyright (c) Cloud Software Group, Inc. 
+ *
  * Redistribution and use in source and binary forms, 
  * with or without modification, are permitted provided 
  * that the following conditions are met: 
@@ -57,7 +56,7 @@ namespace XenAdmin.Diagnostics.Problems.PoolProblem
             _pool,
             BrandManager.ProductVersionPost82
         );
-
+        
         public PoolHasFCoESrWarning(Check check, Pool pool, bool upgradingToVersionWithDeprecation) : base(check)
         {
             _pool = pool;
