1b9978a6c0b77693c5463437b4bad6f403730aa3
Konstantina Chremmou <Konstantina.Chremmou@cloud.com>
2023-04-06 00:31:21 +0100

    CA-376330: If the boot mode radios are invisible do not check them or show the warning if they are disabled.
    Signed-off-by: Konstantina Chremmou <Konstantina.Chremmou@cloud.com>

--- XenAdmin/Wizards/BootModesControl.cs
+++ XenAdmin/Wizards/BootModesControl.cs
@@ -115,7 +115,7 @@ namespace XenAdmin.Wizards
                 return;
             }
 
-            imgUefi.Visible = labelUefi.Visible = true;
+            imgUefi.Visible = labelUefi.Visible = radioButtonUEFIBoot.Visible;
             labelUefi.Text = text;
         }
 
@@ -127,7 +127,7 @@ namespace XenAdmin.Wizards
                 return;
             }
 
-            imgSecureUefi.Visible = labelSecureUefi.Visible = true;
+            imgSecureUefi.Visible = labelSecureUefi.Visible = radioButtonUEFISecureBoot.Visible;
             labelSecureUefi.Text = text;
             imgSecureUefi.Image = isInfo ? Images.StaticImages._000_Info3_h32bit_16 : Images.StaticImages._000_Alert2_h32bit_16;
         }
@@ -174,9 +174,12 @@ namespace XenAdmin.Wizards
                 var secureBoot = _templateVM.GetSecureBootMode();
 
                 if (secureBoot == "true" || secureBoot == "auto" && _poolHasCertificates)
-                    radioButtonUEFISecureBoot.Checked = true;
+                    radioButtonUEFISecureBoot.Checked = radioButtonUEFISecureBoot.Visible;
                 else
-                    radioButtonUEFIBoot.Checked = true;
+                    radioButtonUEFIBoot.Checked = radioButtonUEFIBoot.Visible;
+
+                if (!radioButtonUEFISecureBoot.Checked && !radioButtonUEFIBoot.Checked && radioButtonBIOSBoot.Enabled)
+                    radioButtonBIOSBoot.Checked = true;
 
                 if (radioButtonUEFISecureBoot.Enabled && radioButtonUEFISecureBoot.Checked && !_poolHasCertificates)
                     UpdateSecureUefiWarning(Messages.GUEFI_SECUREBOOT_MODE_MISSING_CERTIFICATES, false);
