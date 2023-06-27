ceaa9abd6631b8dc1d38a0d9d05a09daece12e89
Danilo Del Busso <danilo.delbusso@cloud.com>
2023-06-27 15:40:27 +0100

    CP-43000: Add trial edition and CSS expiry warnings to `LicenseManager`
    Also:
    - Increase size of `LicenseManager` to accommodate larger text
    - Add upsell URL to existing warning for pool size restrictions
    
    Signed-off-by: Danilo Del Busso <danilo.delbusso@cloud.com>

--- XenAdmin/Dialogs/LicenseManager/LicenseStatus.cs
+++ XenAdmin/Dialogs/LicenseManager/LicenseStatus.cs
@@ -29,6 +29,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
@@ -331,31 +332,48 @@ namespace XenAdmin.Dialogs
         {
             get
             {
+                var hosts = XenObject.Connection.Cache.Hosts;
+                var entitlements = new List<string>();
                 if (CurrentState == HostState.Licensed)
                 {
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.EnterpriseFeaturesEnabled()))
-                        return Messages.LICENSE_SUPPORT_AND_ENTERPRISE_FEATURES_ENABLED;
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.DesktopPlusFeaturesEnabled()))
-                        return string.Format(Messages.LICENSE_SUPPORT_AND_DESKTOP_PLUS_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.DesktopFeaturesEnabled()))
-                        return string.Format(Messages.LICENSE_SUPPORT_AND_DESKTOP_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.DesktopCloudFeaturesEnabled()))
-                        return string.Format(Messages.LICENSE_SUPPORT_AND_DESKTOP_CLOUD_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.PremiumFeaturesEnabled()))
-                        return Messages.LICENSE_SUPPORT_AND_PREMIUM_FEATURES_ENABLED;
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.StandardFeaturesEnabled()))
-                        return Messages.LICENSE_SUPPORT_AND_STANDARD_FEATURES_ENABLED;
-                    if (XenObject.Connection.Cache.Hosts.All(h => h.EligibleForSupport()))
-                        return Messages.LICENSE_SUPPORT_AND_STANDARD_FEATURES_ENABLED;
-                    return Messages.LICENSE_NOT_ELIGIBLE_FOR_SUPPORT;
+                    string supportLevel = null;
+                    if (hosts.All(h => h.EnterpriseFeaturesEnabled()))
+                        supportLevel = Messages.LICENSE_ENTERPRISE_FEATURES_ENABLED;
+                    else if (hosts.All(h => h.DesktopPlusFeaturesEnabled()))
+                        supportLevel = string.Format(Messages.LICENSE_DESKTOP_PLUS_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
+                    else if (hosts.All(h => h.DesktopFeaturesEnabled()))
+                        supportLevel = string.Format(Messages.LICENSE_DESKTOP_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
+                    else if (hosts.All(h => h.DesktopCloudFeaturesEnabled()))
+                        supportLevel = string.Format(Messages.LICENSE_DESKTOP_CLOUD_FEATURES_ENABLED, BrandManager.CompanyNameLegacy);
+                    else if (hosts.All(h => h.PremiumFeaturesEnabled()))
+                        supportLevel = Messages.LICENSE_PREMIUM_FEATURES_ENABLED;
+                    else if (hosts.All(h => h.StandardFeaturesEnabled()))
+                        supportLevel = Messages.LICENSE_STANDARD_FEATURES_ENABLED;
+                    else if (hosts.All(h => h.EligibleForSupport()))
+                        supportLevel = Messages.LICENSE_STANDARD_FEATURES_ENABLED;
+
+                    if (supportLevel != null)
+                    {
+                        entitlements.Add(Messages.LICENSE_ELIGIBLE_FOR_SUPPORT);
+                        entitlements.Add(supportLevel);
+                    }
+                    else
+                    {
+                        entitlements.Add(Messages.LICENSE_NOT_ELIGIBLE_FOR_SUPPORT);
+                    }
                 }
 
                 if (CurrentState == HostState.Free)
                 {
-                    return Messages.LICENSE_NOT_ELIGIBLE_FOR_SUPPORT;
+                    entitlements.Add(Messages.LICENSE_NOT_ELIGIBLE_FOR_SUPPORT);
                 }
 
-                return Messages.UNKNOWN;
+                if (hosts.Any(h => h.CanShowTrialEditionUpsell()))
+                    entitlements.Add(Messages.TRIAL_EDITION_UPSELLING_MESSAGE);
+                else if (hosts.Any(h => h.CssLicenseHasExpired()))
+                    entitlements.Add(Messages.EXPIRED_CSS_UPSELLING_MESSAGE_POOL);
+
+                return entitlements.Count == 0 ? Messages.UNKNOWN : string.Join(Environment.NewLine, entitlements);
             }
         }
 
--- XenModel/Messages.Designer.cs
+++ XenModel/Messages.Designer.cs
@@ -16500,6 +16500,24 @@ namespace XenAdmin {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Your Customer Success Services (CSS) agreement has expired. This server will not receive any updates, including security updates.
+        /// </summary>
+        public static string EXPIRED_CSS_UPSELLING_MESSAGE_HOST {
+            get {
+                return ResourceManager.GetString("EXPIRED_CSS_UPSELLING_MESSAGE_HOST", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your Customer Success Services (CSS) agreement has expired. This pool will not receive any updates, including security updates.
+        /// </summary>
+        public static string EXPIRED_CSS_UPSELLING_MESSAGE_POOL {
+            get {
+                return ResourceManager.GetString("EXPIRED_CSS_UPSELLING_MESSAGE_POOL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Export All.
         /// </summary>
         public static string EXPORT_ALL {
@@ -22135,6 +22153,33 @@ namespace XenAdmin {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {0} Virtual Apps and Desktops {0} Cloud features enabled.
+        /// </summary>
+        public static string LICENSE_DESKTOP_CLOUD_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_DESKTOP_CLOUD_FEATURES_ENABLED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} Virtual Apps and Desktops features enabled.
+        /// </summary>
+        public static string LICENSE_DESKTOP_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_DESKTOP_FEATURES_ENABLED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} Virtual Apps and Desktops Premium features enabled.
+        /// </summary>
+        public static string LICENSE_DESKTOP_PLUS_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_DESKTOP_PLUS_FEATURES_ENABLED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to {0} Virtual &amp;Apps and Desktops.
         /// </summary>
         public static string LICENSE_EDITION_DESKTOP {
@@ -22239,6 +22284,24 @@ namespace XenAdmin {
         public static string LICENSE_EDITION_STANDARD_PERSOCKET_LEGACY {
             get {
                 return ResourceManager.GetString("LICENSE_EDITION_STANDARD_PERSOCKET_LEGACY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Eligible for support.
+        /// </summary>
+        public static string LICENSE_ELIGIBLE_FOR_SUPPORT {
+            get {
+                return ResourceManager.GetString("LICENSE_ELIGIBLE_FOR_SUPPORT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Premium features enabled.
+        /// </summary>
+        public static string LICENSE_ENTERPRISE_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_ENTERPRISE_FEATURES_ENABLED", resourceCulture);
             }
         }
         
@@ -22468,6 +22531,15 @@ namespace XenAdmin {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Premium features enabled.
+        /// </summary>
+        public static string LICENSE_PREMIUM_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_PREMIUM_FEATURES_ENABLED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The license server {0} could not be reached. You have until {1} to reconnect to the license server..
         /// </summary>
         public static string LICENSE_REGULAR_GRACE_TOOLTIP {
@@ -22531,71 +22603,20 @@ namespace XenAdmin {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Standard features only.
+        /// </summary>
+        public static string LICENSE_STANDARD_FEATURES_ENABLED {
+            get {
+                return ResourceManager.GetString("LICENSE_STANDARD_FEATURES_ENABLED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Status.
         /// </summary>
         public static string LICENSE_STATUS {
             get {
                 return ResourceManager.GetString("LICENSE_STATUS", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///{0} Virtual Apps and Desktops {0} Cloud features enabled.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_DESKTOP_CLOUD_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_DESKTOP_CLOUD_FEATURES_ENABLED", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///{0} Virtual Apps and Desktops features enabled.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_DESKTOP_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_DESKTOP_FEATURES_ENABLED", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///{0} Virtual Apps and Desktops Premium features enabled.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_DESKTOP_PLUS_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_DESKTOP_PLUS_FEATURES_ENABLED", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///Premium features enabled.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_ENTERPRISE_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_ENTERPRISE_FEATURES_ENABLED", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///Premium features enabled.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_PREMIUM_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_PREMIUM_FEATURES_ENABLED", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Eligible for support 
-        ///Standard features only.
-        /// </summary>
-        public static string LICENSE_SUPPORT_AND_STANDARD_FEATURES_ENABLED {
-            get {
-                return ResourceManager.GetString("LICENSE_SUPPORT_AND_STANDARD_FEATURES_ENABLED", resourceCulture);
             }
         }
         
@@ -26290,7 +26311,7 @@ namespace XenAdmin {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The pool has already reached the maximum number of servers allowed by your license.
+        ///   Looks up a localized string similar to The pool has already reached the maximum number of servers allowed by your license. Go to {0} to purchase licenses.
         /// </summary>
         public static string NEWPOOL_MAX_NUMBER_HOST_REACHED {
             get {
@@ -26326,7 +26347,7 @@ namespace XenAdmin {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The pool will exceed the maximum number of servers allowed by your license.
+        ///   Looks up a localized string similar to The pool will exceed the maximum number of servers allowed by your license. Go to {0} to purchase licenses.
         /// </summary>
         public static string NEWPOOL_WILL_EXCEED_POOL_MAX_SIZE {
             get {
@@ -36540,6 +36561,15 @@ namespace XenAdmin {
         public static string TREESEARCHBOX_DROPDOWN_TOOLTIP {
             get {
                 return ResourceManager.GetString("TREESEARCHBOX_DROPDOWN_TOOLTIP", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Unlicensed servers will not receive any updates, including security updates.
+        /// </summary>
+        public static string TRIAL_EDITION_UPSELLING_MESSAGE {
+            get {
+                return ResourceManager.GetString("TRIAL_EDITION_UPSELLING_MESSAGE", resourceCulture);
             }
         }
         
